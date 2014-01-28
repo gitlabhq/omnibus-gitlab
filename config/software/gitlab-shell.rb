b7efdb58cdfeba0d199a6d1f3b68fc1d37bc205a
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-01-28 15:45:38 +0100

    Install gitlab-shell in service, not services

--- config/software/gitlab-shell.rb
+++ config/software/gitlab-shell.rb
@@ -8,6 +8,6 @@ source :git => "https://gitlab.com/gitlab-org/gitlab-shell.git"
 
 build do
   command "sed -i 's|^#!/usr/bin/env ruby|#!#{install_dir}/embedded/bin/ruby|' $(grep -r -l '#!/usr/bin/env ruby' .)"
-  command "mkdir -p #{install_dir}/embedded/services/gitlab-shell"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/services/gitlab-shell/"
+  command "mkdir -p #{install_dir}/embedded/service/gitlab-shell"
+  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/gitlab-shell/"
 end
