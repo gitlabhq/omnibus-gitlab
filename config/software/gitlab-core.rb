d8484cf2e3d4e8fa4a964b189090fda822547bec
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-02-06 16:48:43 +0100

    Fix a bug in the REVISION hack
    On a fresh omnibus build, when the gitlab-core repo has not been checked
    out yet, the previous code would stop omnibus from loading.

--- config/software/gitlab-core.rb
+++ config/software/gitlab-core.rb
@@ -35,7 +35,7 @@ build do
   # this assumption does not hold. The sed command below patches the GitLab
   # source code to include the Git revision of the code included in the omnibus
   # build.
-  command "sed -i 's/.*REVISION.*/REVISION = \"#{version_guid.split(':').last[0,10]}\"/' config/initializers/2_app.rb"
+  command "sed -i \"s/.*REVISION.*/REVISION = '$(git log --pretty=format:'%h' -n 1)'/\" config/initializers/2_app.rb"
 
   bundle "install --without mysql development test --path=#{install_dir}/embedded/service/gem"
 
