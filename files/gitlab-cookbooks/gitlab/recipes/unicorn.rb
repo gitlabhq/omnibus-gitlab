0e092d47da99eb8b817e230835c20891bc3fcc74
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-02-10 14:33:55 +0100

    Add license header to unicorn.rb

--- files/gitlab-cookbooks/gitlab/recipes/unicorn.rb
+++ files/gitlab-cookbooks/gitlab/recipes/unicorn.rb
@@ -1,3 +1,21 @@
+#
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# Copyright:: Copyright (c) 2014 GitLab.com
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 gitlab_core_dir = node['gitlab']['gitlab-core']['dir']
 gitlab_core_etc_dir = File.join(gitlab_core_dir, "etc")
 gitlab_core_working_dir = File.join(gitlab_core_dir, "working")
