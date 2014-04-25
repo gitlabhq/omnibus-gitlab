f8583ba9a7edf5e45088299ec22806ba612b8ed5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-04-25 13:20:52 +0200

    Add ssl_verify_mode to Chef config
    This will quiet warnings at the start of each `gitlab-ctl reconfigure`
    run.

--- files/gitlab-cookbooks/solo.rb
+++ files/gitlab-cookbooks/solo.rb
@@ -1,4 +1,5 @@
 CURRENT_PATH = File.expand_path(File.dirname(__FILE__))
 file_cache_path "#{CURRENT_PATH}/cache"
-cookbook_path CURRENT_PATH 
+cookbook_path CURRENT_PATH
 verbose_logging false
+ssl_verify_mode :verify_peer
