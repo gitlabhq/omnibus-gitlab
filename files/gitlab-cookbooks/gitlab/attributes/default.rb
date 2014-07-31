c6c8dafd9aa78777e87423f5e1dbb66f004992d7
Dennis Markgraf <dennis@nerdindustries.com>
2014-07-31 14:42:26 +0200

    Adds smtp_openssl_verify_mode default value

--- files/gitlab-cookbooks/gitlab/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab/attributes/default.rb
@@ -144,6 +144,7 @@ default['gitlab']['gitlab-rails']['smtp_domain'] = nil
 default['gitlab']['gitlab-rails']['smtp_authentication'] = nil
 default['gitlab']['gitlab-rails']['smtp_enable_starttls_auto'] = nil
 default['gitlab']['gitlab-rails']['smtp_tls'] = nil
+default['gitlab']['gitlab-rails']['smtp_openssl_verify_mode'] = nil
 
 ####
 # Unicorn
