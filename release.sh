f4712547fcb46f43dc1ae7f421516acac5e7ba88
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-08-20 17:18:22 +0200

    Run `bundle install` in release.sh

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@
 # Generate a build ID based on the current time and the PID of this script
 build="$(date '+%s')-$$"
 
+# Install/update gems for omnibus-ruby
+bundle install
+
 # Do the build and capture its output in a .log file
 make do_release 2>&1 | tee -a ${build}.log
 
