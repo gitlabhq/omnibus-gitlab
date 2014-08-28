a54043f8b01e4a073b97be831b180b8452f30a1d
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-08-28 13:20:30 +0200

    Build EE packages from dev.gitlab.org

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -33,7 +33,7 @@ dependency "python-docutils"
 dependency "mysql-client" if EE
 dependency "rugged"
 
-source :git => "git@gitlab.com:subscribers/gitlab-ee.git"
+source :git => "git@dev.gitlab.org:gitlab/gitlab-ee.git"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
