d4fa7df0656b773cf6aed8d452dd181e48f7a48a
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-08-28 16:14:13 +0200

    Simplify EE check script
    No need to be too specific about the git remote.

--- support/is_gitlab_ee.sh
+++ support/is_gitlab_ee.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ -n $ee ]] || grep -q '^source.*"git@gitlab.com:subscribers/gitlab-ee.git"' config/software/gitlab-rails.rb
+[[ -n $ee ]] || grep -q '^source.*gitlab-ee.git"' config/software/gitlab-rails.rb
