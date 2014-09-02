1ac8ea99e0326162d6a6f279caa36fdf4b435010
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-09-02 13:52:54 +0200

    Remove unnecessary code from nginx recipe
    The nginx_vars hash already contains all variables from
    node['gitlab']['nginx'].

--- files/gitlab-cookbooks/gitlab/recipes/nginx.rb
+++ files/gitlab-cookbooks/gitlab/recipes/nginx.rb
@@ -48,10 +48,6 @@ template nginx_vars[:gitlab_http_config] do
       :https => node['gitlab']['gitlab-rails']['gitlab_https'],
       :socket => node['gitlab']['unicorn']['socket'],
       :port => node['gitlab']['gitlab-rails']['gitlab_port'],
-      :redirect_http_to_https => node['gitlab']['nginx']['redirect_http_to_https'],
-      :redirect_http_to_https_port => node['gitlab']['nginx']['redirect_http_to_https_port'],
-      :ssl_certificate => node['gitlab']['nginx']['ssl_certificate'],
-      :ssl_certificate_key => node['gitlab']['nginx']['ssl_certificate_key']
     }
   ))
   notifies :restart, 'service[nginx]' if OmnibusHelper.should_notify?("nginx")
