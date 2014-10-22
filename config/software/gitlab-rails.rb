8fe2d6bc34d839d7db87c095863824104d9053b3
Marin Jankovski <maxlazio@gmail.com>
2014-10-22 15:52:17 +0200

    GitLab CE 7.4.0 with gitlab-shell 2.0.1.

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -17,7 +17,7 @@
 #
 
 name "gitlab-rails"
-default_version "630d042433e0cecffe80aa5ebd7aa7a91c5eb2d8" # 7.3.1.rc1
+default_version "ba76dbc3667c2eb0a1a3687f8b0481e619946d73" # 7.4.0
 
 EE = system("#{Config.project_root}/support/is_gitlab_ee.sh")
 
