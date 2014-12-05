e1f52503f634e6c76a37e9dbf65528ad0577f32b
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-12-05 11:48:11 +0100

    Build gitlab-rails 7.5.3-ee

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -17,7 +17,7 @@
 #
 
 name "gitlab-rails"
-default_version "58eabe4ec875d355317b2d3ef5ee84a52523b04e" # 7.5.2-ee
+default_version "6358d7732b403cfc1c01745e9b3f94516f0781ca" # 7.5.3-ee
 
 EE = system("#{Config.project_root}/support/is_gitlab_ee.sh")
 
