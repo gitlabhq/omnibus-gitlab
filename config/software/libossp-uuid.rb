29129bf24c4de045ab0143b0ff8772d9330b78c0
DJ Mountney <david@twkie.net>
2015-04-17 08:57:34 -0700

    The mirror currently set in omnibus software points to an ftp is fairly flakey and limited.
    There is an open merge request against omnibus software with this update to source:
    https://github.com/chef/omnibus-software/pull/383

---
+++ config/software/libossp-uuid.rb
@@ -0,0 +1,38 @@
+#
+# Copyright 2012-2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "libossp-uuid"
+default_version "1.6.2"
+
+version "1.6.2" do
+  source md5: "5db0d43a9022a6ebbbc25337ae28942f"
+end
+
+# ftp on ftp.ossp.org is unavaiable so we must use another mirror site.
+# From homebrew discussion, following url is better for now: https://github.com/Homebrew/homebrew/pull/14288
+source url: "http://gnome-build-stage-1.googlecode.com/files/uuid-#{version}.tar.gz"
+
+relative_path "uuid-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded", env: env
+
+  make "-j #{workers}", env: env
+  make "install", env: env
+end
