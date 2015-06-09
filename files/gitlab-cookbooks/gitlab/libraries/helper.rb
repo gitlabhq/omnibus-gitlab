757aced29ac03df97c36273380f50e20611ba9f9
Marin Jankovski <maxlazio@gmail.com>
2015-06-09 14:37:35 +0200

    Move secrets read and write to a helper.

--- files/gitlab-cookbooks/gitlab/libraries/helper.rb
+++ files/gitlab-cookbooks/gitlab/libraries/helper.rb
@@ -93,44 +93,97 @@ end
 class CiHelper
 
   def self.authorize_with_gitlab(gitlab_external_url)
-    require 'yaml'
-    credentials_file = "/var/opt/gitlab/gitlab-ci/etc/gitlab_server.yml"
+    credentials_file = "/etc/gitlab/gitlab-secrets.json"
 
-    if File.exists?(credentials_file)
-      Chef::Log.debug("Reading the CI credentials file at #{credentials_file}")
-      Chef::Log.debug("If you need to change app_id and app_secret use gitlab.rb.")
-      gitlab_server = YAML::load_file(credentials_file)
-      app_id = gitlab_server[:app_id]
-      app_secret = gitlab_server[:app_secret]
+    Chef::Log.warn("Connecting to GitLab to generate new app_id and app_secret.")
+    runner_cmd = [
+      "app=Doorkeeper::Application.where(redirect_uri: \"#{gitlab_external_url}\", name: \"GitLab CI\").first_or_create",
+      "puts app.uid.concat(\" \").concat(app.secret);"
+      ].join(" ;")
+
+    cmd = [
+      '/opt/gitlab/bin/gitlab-rails',
+      'runner',
+      '-e production',
+      "\'#{runner_cmd}\'"
+    ].join(" ")
+
+    o = Mixlib::ShellOut.new(cmd)
+    o.run_command
+
+    app_id, app_secret = nil
+    if o.exitstatus == 0
+      app_id, app_secret = o.stdout.chomp.split(" ")
+
+      Gitlab['gitlab_ci']['gitlab_server'] = { 'url' => gitlab_external_url,
+                                                 'app_id' => app_id,
+                                                 'app_secret' => app_secret
+                                               }
+
+      SecretsHelper.write_to_gitlab_secrets
+
+      Chef::Log.info("Updated the #{credentials_file} file.")
     else
-      Chef::Log.debug("Didn't find #{credentials_file}, connecting to database to generate new app_id and app_secret.")
-      runner_cmd = [
-        "app=Doorkeeper::Application.where(redirect_uri: \"#{gitlab_external_url}\", name: \"GitLab CI\").first_or_create",
-        "puts app.uid.concat(\" \").concat(app.secret);"
-        ].join(" ;")
-
-      cmd = [
-        '/opt/gitlab/bin/gitlab-rails',
-        'runner',
-        '-e production',
-        "\'#{runner_cmd}\'"
-      ].join(" ")
-
-      o = Mixlib::ShellOut.new(cmd)
-      o.run_command
-
-      app_id, app_secret = nil
-      if o.exitstatus == 0
-        app_id, app_secret = o.stdout.chomp.split(" ")
-        gitlab_server = { app_id: app_id, app_secret: app_secret }
-        File.open(credentials_file, 'w') { |file| file.write gitlab_server.to_yaml }
-        Chef::Log.debug("Created the CI credentials file at #{credentials_file}")
-      else
-        Chef::Log.warn("Something went wrong while trying to create #{credentials_file}. Check the file permissions and try reconfiguring again.")
-      end
+      Chef::Log.warn("Something went wrong while trying to update #{credentials_file}. Check the file permissions (default 600) and try reconfiguring again.")
     end
 
     { 'url' => gitlab_external_url, 'app_id' => app_id, 'app_secret' => app_secret }
+  end
+
+end
+
+class SecretsHelper
+
+  def self.read_gitlab_secrets
+    existing_secrets ||= Hash.new
+
+    if File.exists?("/etc/gitlab/gitlab-secrets.json")
+      existing_secrets = Chef::JSONCompat.from_json(File.read("/etc/gitlab/gitlab-secrets.json"))
+    end
+
+    existing_secrets.each do |k, v|
+      v.each do |pk, p|
+        # Note: Specifiying a secret in gitlab.rb will take precendence over the `gitlab-secrets.json`
+        Gitlab[k][pk] ||= p
+      end
+    end
+  end
+
+  def self.write_to_gitlab_secrets
+    secret_tokens = {
+                      'gitlab_shell' => {
+                        'secret_token' => Gitlab['gitlab_shell']['secret_token'],
+                      },
+                      'gitlab_rails' => {
+                        'secret_token' => Gitlab['gitlab_rails']['secret_token'],
+                      },
+                      'gitlab_ci' => {
+                        'secret_token' => Gitlab['gitlab_ci']['secret_token'],
+                      }
+                    }
+
+    ci_credentials = if Gitlab['gitlab_ci']['gitlab_server']
+       { 'gitlab_ci' => {
+                          'secret_token' => Gitlab['gitlab_ci']['secret_token'],
+                          'gitlab_server' => {
+                            'url' => Gitlab['gitlab_ci']['gitlab_server']['url'],
+                            'app_id' => Gitlab['gitlab_ci']['gitlab_server']['app_id'],
+                            'app_secret' => Gitlab['gitlab_ci']['gitlab_server']['app_secret']
+                          }
+                        }
+        }
+    else
+      {}
+    end
+
+    if File.directory?("/etc/gitlab")
+      File.open("/etc/gitlab/gitlab-secrets.json", "w") do |f|
+        f.puts(
+          Chef::JSONCompat.to_json_pretty(secret_tokens.merge(ci_credentials))
+        )
+        system("chmod 0600 /etc/gitlab/gitlab-secrets.json")
+      end
+    end
   end
 
 end
