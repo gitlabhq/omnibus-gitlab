18df2dad8e25d89a04a78043ee07ee485a7dab80
Marin Jankovski <maxlazio@gmail.com>
2015-08-20 14:26:23 +0200

    Add mattermost prefix to svlogs.

--- files/gitlab-cookbooks/gitlab/libraries/gitlab.rb
+++ files/gitlab-cookbooks/gitlab/libraries/gitlab.rb
@@ -166,6 +166,7 @@ module Gitlab
         postgresql
         remote-syslog
         gitlab-git-http-server
+        mattermost
       }.each do |runit_sv|
         Gitlab[runit_sv.gsub('-', '_')]['svlogd_prefix'] ||= "#{node['hostname']} #{runit_sv}: "
       end
