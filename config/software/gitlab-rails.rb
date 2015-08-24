292160af2c957ec9c6c5e9d231f9b3f9b3ff3278
Marin Jankovski <maxlazio@gmail.com>
2015-08-24 13:40:54 +0200

    Pin to master.

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -17,7 +17,7 @@
 #
 
 name "gitlab-rails"
-default_version "f5483791e5bfd8b51e985e0f8087c44715dc2eb4" # v7.14.0
+default_version "master" # Nightly build
 
 EE = system("#{Omnibus::Config.project_root}/support/is_gitlab_ee.sh")
 
