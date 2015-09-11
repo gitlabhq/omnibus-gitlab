d3f5f0cfb7d355f781472831437980194698777c
Marin Jankovski <maxlazio@gmail.com>
2015-09-11 10:47:53 +0200

    Add GitLab Mattermost license. Bump Mattermost version to 0.7.1

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -17,10 +17,10 @@
 #
 
 name "mattermost"
-default_version "v0.7.0"
+default_version "v0.7.1"
 
 source url: "https://github.com/mattermost/platform/releases/download/#{version}/mattermost.tar.gz",
-       md5: '92348d916d80a8f288f2507665f89349'
+       md5: '0d5329fadb87dfe60a6b9a189782c3b6'
 
 build do
   move "bin/platform", "#{install_dir}/embedded/bin/mattermost"
@@ -29,4 +29,25 @@ build do
   command "#{install_dir}/embedded/bin/rsync -a --delete ./api/templates #{install_dir}/embedded/service/mattermost/api/"
   command "#{install_dir}/embedded/bin/rsync -a --delete ./web/static #{install_dir}/embedded/service/mattermost/web/"
   command "#{install_dir}/embedded/bin/rsync -a --delete ./web/templates #{install_dir}/embedded/service/mattermost/web/"
+
+  license_name = "GITLAB-MATTERMOST-COMPILED-LICENSE.txt"
+  license_path = File.join(install_dir, "embedded/service/mattermost", license_name)
+
+  File.open(license_path, 'w') { |f| f.write(GITLAB_MATTERMOST_COMPILED_LICENSE) }
 end
+
+GITLAB_MATTERMOST_COMPILED_LICENSE = <<-EOH
+
+GitLab Mattermost Compiled License
+(MIT with Trademark Protection)
+
+**Note: this license does not cover source code, for information on source code licensing see http://www.mattermost.org/license/
+
+Copyright (c) 2015 Spinpunch, Inc.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software;
+The receiver of the Software will not remove or alter any product identification, trademark, copyright or other notices embedded within or appearing within or on the Software;
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+EOH
