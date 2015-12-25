1c6bcf47a945af21651f83dcdd35109c9c619e2a
Marin Jankovski <maxlazio@gmail.com>
2015-12-25 10:58:40 +0100

    Upgrade rubygems to 2.5.1.

--- config/projects/gitlab.rb
+++ config/projects/gitlab.rb
@@ -46,7 +46,7 @@ build_version   Omnibus::BuildVersion.new.semver
 build_iteration Gitlab::BuildIteration.new.build_iteration
 
 override :ruby, version: '2.1.7'
-override :rubygems, version: 'ccfafdc2c52c5c605ff69ed3a772d83eb19ef55a' # 2.2.5
+override :rubygems, version: 'v2.5.1'
 override :'chef-gem', version: '12.5.1'
 override :cacerts, version: '2015.10.28', source: { md5: '6f41fb0f0c4b4695c2a6296892278141' }
 override :openssl, version: '1.0.1q', source: { url: 'https://www.openssl.org/source/openssl-1.0.1q.tar.gz', md5: '54538d0cdcb912f9bc2b36268388205e' }
