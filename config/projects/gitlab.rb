9dcfe5cbb4fbf89c8397cd8aee5a3997845a5342
Marin Jankovski <maxlazio@gmail.com>
2016-01-20 11:41:16 +0100

    Cacerts 20.01.2016.

--- config/projects/gitlab.rb
+++ config/projects/gitlab.rb
@@ -48,7 +48,7 @@ build_iteration Gitlab::BuildIteration.new.build_iteration
 override :ruby, version: '2.1.8', source: { md5: '091b62f0a9796a3c55de2a228a0e6ef3' }
 override :rubygems, version: 'v2.5.1'
 override :'chef-gem', version: '12.5.1'
-override :cacerts, version: '2015.10.28', source: { md5: '6f41fb0f0c4b4695c2a6296892278141' }
+override :cacerts, version: '2016.01.20', source: { md5: '06629db7f712ff3a75630eccaecc1fe4' }
 override :openssl, version: '1.0.1q', source: { url: 'https://www.openssl.org/source/openssl-1.0.1q.tar.gz', md5: '54538d0cdcb912f9bc2b36268388205e' }
 override :pip, version: '7.1.2', source: { md5: '3823d2343d9f3aaab21cf9c917710196' }
 
