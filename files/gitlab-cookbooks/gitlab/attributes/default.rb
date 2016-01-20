f8ee4a19ede081f89811eb75fae4bb320e06458b
Yorick Peterse <yorickpeterse@gmail.com>
2016-01-20 11:59:49 +0100

    Increase Unicorn memory limits to 300-350
    See gitlab-org/gitlab-ce!2513 and gitlab-org/gitlab-ce!2421 for more
    information.

--- files/gitlab-cookbooks/gitlab/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab/attributes/default.rb
@@ -257,8 +257,8 @@ default['gitlab']['unicorn']['tcp_nopush'] = true
 default['gitlab']['unicorn']['backlog_socket'] = 1024
 default['gitlab']['unicorn']['somaxconn'] = 1024
 default['gitlab']['unicorn']['worker_timeout'] = 60
-default['gitlab']['unicorn']['worker_memory_limit_min'] = "250*(1024**2)"
-default['gitlab']['unicorn']['worker_memory_limit_max'] = "300*(1024**2)"
+default['gitlab']['unicorn']['worker_memory_limit_min'] = "300*(1024**2)"
+default['gitlab']['unicorn']['worker_memory_limit_max'] = "350*(1024**2)"
 
 ####
 # Sidekiq
