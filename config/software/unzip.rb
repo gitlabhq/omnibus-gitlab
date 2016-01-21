067dfe36badcaee5be125e16c3e7a472c206f9fb
Marin Jankovski <marin@gitlab.com>
2016-01-21 15:11:05 +0000

    Merge branch 'bundle_unzip' into 'master'
    Bundle unzip.
    
    Fixes #1057 
    
    See merge request !607

---
+++ config/software/unzip.rb
@@ -0,0 +1,33 @@
+#
+## Copyright:: Copyright (c) 2016 GitLab Inc
+## License:: Apache License, Version 2.0
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+#
+
+name "unzip"
+default_version "6.0"
+
+source :url => "http://vorboss.dl.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz",
+       :md5 => "62b490407489521db863b523a7f86375"
+
+relative_path "unzip60"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  make "-f unix/Makefile clean", env: env
+  make "-j #{workers} -f unix/Makefile generic", env: env
+  make "-f unix/Makefile prefix=#{install_dir}/embedded install", env: env
+end
