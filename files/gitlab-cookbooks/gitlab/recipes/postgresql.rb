b717fcb84bc77f00ac83e638f83e537173acb3be
Marin Jankovski <maxlazio@gmail.com>
2016-03-14 12:16:37 +0100

    Trigger enabling pg_trgm extension.

--- files/gitlab-cookbooks/gitlab/recipes/postgresql.rb
+++ files/gitlab-cookbooks/gitlab/recipes/postgresql.rb
@@ -149,20 +149,22 @@ if node['gitlab']['gitlab-rails']['enable']
     not_if { !pg_helper.is_running? || pg_helper.user_exists?(gitlab_sql_user) }
   end
 
-  execute "enable pg_trgm extension" do
-    command "#{bin_dir}/psql --port #{pg_port} -h #{postgresql_socket_dir} -d template1 -c \"CREATE EXTENSION IF NOT EXISTS pg_trgm;\""
-    user postgresql_user
-    retries 20
-    not_if { !pg_helper.is_running? }
-  end
-
   execute "create #{database_name} database" do
     command "#{bin_dir}/createdb --port #{pg_port} -h #{postgresql_socket_dir} -O #{gitlab_sql_user} #{database_name}"
     user postgresql_user
     retries 30
+    notifies :run, "execute[enable pg_trgm extension]", :immediately
     notifies :run, "execute[initialize gitlab-rails database]", :immediately
     not_if { !pg_helper.is_running? || pg_helper.database_exists?(database_name) }
   end
+end
+
+execute "enable pg_trgm extension" do
+  command "#{bin_dir}/psql --port #{pg_port} -h #{postgresql_socket_dir} -d #{database_name} -c \"CREATE EXTENSION IF NOT EXISTS pg_trgm;\""
+  user postgresql_user
+  retries 20
+  action :nothing
+  not_if { !pg_helper.is_running? }
 end
 
 ###
