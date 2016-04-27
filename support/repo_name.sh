d3b0aa2d17f11bcf961eb9d0985f5b64d5a01366
Marin Jankovski <maxlazio@gmail.com>
2016-04-27 08:58:19 +0200

    Push packages to a separate security repo.

--- support/repo_name.sh
+++ support/repo_name.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if git describe | grep -q -e rc ; then
-  echo unstable;
-elif support/is_gitlab_ee.sh ; then
-  echo gitlab-ee;
-else
-  echo gitlab-ce;
-fi
+# if git describe | grep -q -e rc ; then
+#   echo unstable;
+# elif support/is_gitlab_ee.sh ; then
+#   echo gitlab-ee;
+# else
+#   echo gitlab-ce;
+# fi
+echo security-920788061529126
