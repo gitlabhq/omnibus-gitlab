a1e9d782d25e35c7322655201473dd4162311e85
Marin Jankovski <maxlazio@gmail.com>
2016-05-03 12:18:15 +0200

    Revert "Push packages to a separate security repo."
    This reverts commit c62c5fa15ffa7eac6e9958177e3eaf0b0ce7b790.

--- support/repo_name.sh
+++ support/repo_name.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# if git describe | grep -q -e rc ; then
-#   echo unstable;
-# elif support/is_gitlab_ee.sh ; then
-#   echo gitlab-ee;
-# else
-#   echo gitlab-ce;
-# fi
-echo security-920788061529126
+if git describe | grep -q -e rc ; then
+  echo unstable;
+elif support/is_gitlab_ee.sh ; then
+  echo gitlab-ee;
+else
+  echo gitlab-ce;
+fi
