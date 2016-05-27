a5fc79bff558127ac8a553e264b62fbb2ef718d8
Jacob Vosmaer <jacob@gitlab.com>
2016-05-27 12:35:57 +0200

    Have to use default_version?

--- config/software/gitlab-psql.rb
+++ config/software/gitlab-psql.rb
@@ -21,7 +21,7 @@ name "gitlab-psql"
 
 # This 'software' is self-contained in this file. Use the file contents
 # to generate a version string.
-version Digest::MD5.file(__FILE__).hexdigest
+default_version Digest::MD5.file(__FILE__).hexdigest
 
 build do
   block do
