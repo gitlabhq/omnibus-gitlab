f7571ccbd8198e3a6883c1cd2eb40460f3db9568
Marin Jankovski <maxlazio@gmail.com>
2016-07-06 19:40:01 -0400

    Add version to version server after package build on tag succeeded.

--- support/add_to_version_server.sh
+++ support/add_to_version_server.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # Set version to version.gitlab.com
-# Env. variable CI_VERSION_TOKEN, CI_VERSION_API_URL and CI_BUILD_REF_NAME need
-# to be set
+# Variable VERSION_TOKEN and CI_BUILD_TAG need to be set
 
-BUILD_REF_NAME=$1
-CI_VERSION_API_URL=$2
-CI_VERSION_TOKEN=$3
+CI_BUILD_TAG=$1
+VERSION_TOKEN=$2
 
-VERSION=$(echo $BUILD_REF_NAME | grep -o -v "+rc" | grep -o -E "^[0-9]+\.[0-9]\.[0-9]")
-if [ -z "$BUILD_REF_NAME" ] || [ -z "$CI_VERSION_API_URL" ] || [ -z "$CI_VERSION_TOKEN" ]; then
-    echo "Missing one of the arguments! (options: BUILD_REF_NAME, CI_VERSION_API_URL, CI_VERSION_TOKEN)"
+VERSION=$(echo $CI_BUILD_TAG | grep -o -v "+rc" | grep -o -E "^[0-9]+\.[0-9]\.[0-9]")
+if [ -z "$CI_BUILD_TAG" ] || [ -z "$VERSION_TOKEN" ]; then
+    echo "Missing one of the arguments! (options: CI_BUILD_TAG, VERSION_TOKEN)"
     exit 1
 else
   if [ -z "$VERSION" ]; then
-      echo "$BUILD_REF_NAME not eligible for adding to version.gitlab.com"
+      echo "$CI_BUILD_TAG not eligible for adding to version.gitlab.com"
   else
-      echo "Matched $VERSION from $BUILD_REF_NAME"
-      curl -X POST --header "Content-Type application/json" -d "private_token=$CI_VERSION_TOKEN" -d "version=$VERSION" "$CI_VERSION_API_URL/versions/"
+      echo "Matched $VERSION from $CI_BUILD_TAG"
+      curl -X POST --header "Content-Type application/json" -d "private_token=$VERSION_TOKEN" -d "version=$VERSION" "https://version.gitlab.com/api/v1/versions/"
   fi
 fi
