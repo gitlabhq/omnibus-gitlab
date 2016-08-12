6f9951faeeb8c298e7b9cdcb0faed829d86531e3
Marin Jankovski <maxlazio@gmail.com>
2016-08-12 15:35:03 +0200

    Add registry recipe basic specs, fix deprecation warnings.

--- spec/chef/gitlab_shell_spec.rb
+++ spec/chef/gitlab_shell_spec.rb
@@ -5,13 +5,6 @@ describe 'gitlab_shell' do
 
   before { allow(Gitlab).to receive(:[]).and_call_original }
 
-  # def stub_gitlab_rb(config)
-  #   config.each do |key, value|
-  #     value = Mash.from_hash(value) if value.is_a?(Hash)
-  #     allow(Gitlab).to receive(:[]).with(key.to_s).and_return(value)
-  #   end
-  # end
-
   context 'when git_data_dir is set as a single directory' do
     before { stub_gitlab_rb(git_data_dir: '/tmp/user/git-data') }
 
@@ -46,11 +39,6 @@ describe 'gitlab_shell' do
         'overflow' => '/tmp/other/git-overflow-data/repositories'
       })
     end
-  end
-
-  it 'defaults the auth_file to be within the user\'s home directory' do
-    stub_gitlab_rb(user: { home: '/tmp/user' })
-    expect(chef_run.node['gitlab']['gitlab-shell']['auth_file']).to eq('/tmp/user/.ssh/authorized_keys')
   end
 
   it 'uses custom auth_files set in gitlab.rb' do
