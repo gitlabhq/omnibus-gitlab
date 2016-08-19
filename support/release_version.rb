03c4fc39dbd071325b33227f1fd118adfb6e53ad
Marin Jankovski <maxlazio@gmail.com>
2016-08-19 16:59:41 +0200

    Update version for docker build.

--- support/release_version.rb
+++ support/release_version.rb
@@ -5,4 +5,9 @@ Omnibus.load_configuration('omnibus.rb')
 
 require "#{Omnibus::Config.project_root}/lib/gitlab/build_iteration"
 
-puts "#{Omnibus::BuildVersion.semver}-#{Gitlab::BuildIteration.new.build_iteration}"
+semver = Omnibus::BuildVersion.semver
+if ENV['NIGHTLY'] && ENV['CI_PIPELINE_ID']
+  semver = "#{semver}.#{ENV['CI_PIPELINE_ID']}"
+end
+
+puts "#{semver}-#{Gitlab::BuildIteration.new.build_iteration}"
