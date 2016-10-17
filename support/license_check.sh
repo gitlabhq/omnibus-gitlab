21f4d04ea442c2431e009859d270264efeb1fe73
Jason Plum <jplum@gitlab.com>
2016-10-17 07:54:27 -0400

    Update license_check.sh: catch unknown
    Update license_check.sh to check good/bad/unknown for each license.
    - Fix some comment language
    - Report uknown licenses (not in good or bad lists)
    - Add '^$' to badlist.

--- support/license_check.sh
+++ support/license_check.sh
@@ -1,14 +1,12 @@
 #!/bin/env bash
 # license_check.sh
 # Check the included software licenses, as deliniated in $install_dir/LICENSE
-# - Compare to list of good/bad, and throw warnings (for now)
+# - Compare to list of good/bad/unknown, and throw warnings (for now)
 # Note: this is currently heavily related to the state of omnibus' licensing.rb
 
 # list arrays, members are pattern strings!
-# GOOD currently not actually used in this code, and here for documentation reasons.
 GOODLIST=('^MIT' '^LGPL' '^Apache' '^Ruby' '^BSD-[23]{1}' '^ISC' )
 BADLIST=('^GPL' '^AGPL' )
-
 
 # fetch install_dir from config/projects/gitlab.rb and verify the output.
 install_dir="$(grep -B0 -A0 -C1 -e '^install_dir' config/projects/gitlab.rb | cut -d'"' -f2)"
@@ -27,7 +25,7 @@ declare -A LICENSE
     license=''
     while IFS= read -r line ; 
     do
-        # reset to be sure we don't somehow accidentally 
+        # reset to be sure we don't accidentally fill erroneously
         if [[ "$line" == "--" ]]; then
             software=''
             license=''
@@ -47,11 +45,31 @@ declare -A LICENSE
 }
 
 # check the license against the pattern from the lists.
+# - for x in seq: we have two arrays, and need to walk synchronously
 for x in `seq 0 "$(( ${#SOFTWARE[*]} - 1 ))"`; do
-    for n in ${BADLIST[@]} ; do
+    # managed continue state once we've checked a license.
+    CONTINUE=false
+
+    # if it matches in GOODLIST, break this loop, and continue in the parent
+    for n in ${GOODLIST[@]} ; do
         if [[ ${LICENSE[$x]} =~ $n ]]; then
-            echo "BAD LICENSE! ${SOFTWARE[$x]} uses ${LICENSE[$x]}"
-            continue
+            echo "Good   : ${SOFTWARE[$x]} uses ${LICENSE[$x]}"
+            CONTINUE=true;
+            break;
         fi
     done
-done 
+    if [[ $CONTINUE == true ]]; then continue; fi
+
+    # if it matches in BADLIST, break this loop, and continue in the parent
+    for n in ${BADLIST[@]} ; do
+        if [[ ${LICENSE[$x]} =~ $n ]]; then
+            echo "Check  ! ${SOFTWARE[$x]} uses ${LICENSE[$x]}"
+            CONTINUE=true
+            break;
+        fi
+    done
+    if [[ $CONTINUE == true ]]; then continue; fi
+
+    # if we've made it here, we're unsure of the state of the reported license
+    echo "Unknown? ${SOFTWARE[$x]} uses ${LICENSE[$x]}"
+done
