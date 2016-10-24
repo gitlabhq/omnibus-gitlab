98f4cadcc4136e9bd2414ca0fe66939d8d7b026a
Marin Jankovski <maxlazio@gmail.com>
2016-10-24 17:38:39 +0200

    Update docs for nginx status, fix the default server for status config.

--- spec/chef/nginx_spec.rb
+++ spec/chef/nginx_spec.rb
@@ -100,6 +100,7 @@ describe 'nginx' do
         "port" => 8060,
         "options" => {
           "stub_status" => "on",
+          "server_tokens" => "off",
           "access_log" => "off",
           "allow" => "127.0.0.1",
           "deny" => "all"
@@ -116,6 +117,7 @@ describe 'nginx' do
         "port" => 9999,
         "options" => {
           "stub_status" => "on",
+          "server_tokens" => "off",
           "access_log" => "on",
           "allow" => "127.0.0.1",
           "deny" => "all"
