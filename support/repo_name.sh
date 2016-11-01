d1352709aa9cd672a8a7401164b1f0786fb8246d
DJ Mountney <dj@gitlab.com>
2016-11-01 16:17:51 +0000

    Merge branch 'repository_name_for_package_push' into 'master'
    Allow from one location a full override of where all packages are being sent to.
    
    Allows us to do an override of where all packages are being sent to.
    
    cc @twk3 @ibaum @brodock @WarheadsSE @stanhu
    
    See merge request !1073

--- support/repo_name.sh
+++ support/repo_name.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-if git describe | grep -q -e rc ; then
-  echo unstable;
-elif support/is_gitlab_ee.sh ; then
-  echo gitlab-ee;
+# Order of priority:
+## PACKAGECLOUD_REPO is defined, forward everything to that repository. This is a full override.
+## RASPBERRY_REPO is defined, forward to raspberry-pi2 repository
+## NIGHTLY_REPO is defined, forward to nightly-builds repository
+## No repo defined, select based on git describe. Unstable, gitlab-ee or gitlab-ce
+if [ -z ${PACKAGECLOUD_REPO+x} ]; then
+  if [ -z ${RASPBERRY_REPO+x} ]; then
+    if [ -z ${NIGHTLY_REPO+x} ]; then
+      if git describe | grep -q -e rc ; then
+        echo unstable;
+      elif support/is_gitlab_ee.sh ; then
+        echo gitlab-ee;
+      else
+        echo gitlab-ce;
+      fi
+    else
+      echo $NIGHTLY_REPO;
+    fi
+  else
+    echo $RASPBERRY_REPO;
+  fi
 else
-  echo gitlab-ce;
+  echo $PACKAGECLOUD_REPO;
 fi
