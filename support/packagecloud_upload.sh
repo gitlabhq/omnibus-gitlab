4e2b34d94e00f3b4c26fa90254cda42d44a9fbeb
Jason Plum <jplum@gitlab.com>
2016-11-11 12:16:58 -0500

    packagecloud_upload.sh Verify argument count
    Update `packagecloud_upload.sh` to account for checking the argument count.
    
    *  also fixed white space on L30 for integer math vs string handling. `expr` is used here for older systems that might have an older `bash`.

--- support/packagecloud_upload.sh
+++ support/packagecloud_upload.sh
@@ -8,6 +8,11 @@
 # - We set LC_ALL below because package_cloud is picky about the locale
 export LC_ALL='en_US.UTF-8'
 
+if [ $# -ne 3 ]; then
+    echo "FAILURE: Invalid number of arguments. Got $#, '$@'"
+    exit 1;
+fi
+
 PACKAGECLOUD_USER=$1
 PACKAGECLOUD_REPO=$2
 PACKAGECLOUD_OS=$3
@@ -27,7 +32,7 @@ for distro in "${OS[@]}" ; do
     # chance that it occurs, we'll output a warning, and then attempt upload anyways
     count=0
     for package in `find pkg -name '*.rpm' -o -name '*.deb'`; do
-        count=$(expr $count+1)
+        count=$(expr $count + 1)
         if [ $count -gt 1 ]; then
             echo "WARNING: multiple packages detected!"
         fi
