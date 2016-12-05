c72e3928fa0cc3cc932d917d5f5d29e4873f7233
Balasankar C <balasankarc@autistici.org>
2016-12-05 09:52:58 +0530

    Add sidekiq specs - User specified values

--- spec/chef/recipes/sidekiq_spec.rb
+++ spec/chef/recipes/sidekiq_spec.rb
@@ -10,6 +10,18 @@ describe 'gitlab::sidekiq' do
   context 'with default values' do
     it 'correctly renders out the sidekiq service file' do
         expect(chef_run).to render_file("/opt/gitlab/sv/sidekiq/run").with_content(/\-C \/opt\/gitlab\/embedded\/service\/.*\/config\/sidekiq_queues.yml/)
+        expect(chef_run).to render_file("/opt/gitlab/sv/sidekiq/run").with_content(/\-t 4/)
+        expect(chef_run).to render_file("/opt/gitlab/sv/sidekiq/run").with_content(/\-c 25/)
+    end
+  end
+
+  context 'with specified values' do
+    before do
+      stub_gitlab_rb(sidekiq: { shutdown_timeout: 8, concurrency: 35})
+    end
+    it 'correctly renders out the sidekiq service file' do
+        expect(chef_run).to render_file("/opt/gitlab/sv/sidekiq/run").with_content(/\-t 8/)
+        expect(chef_run).to render_file("/opt/gitlab/sv/sidekiq/run").with_content(/\-c 35/)
     end
   end
 end
