2fae00d560c769cdd0e189adc43328bd5d50ab8b
Gabriel Mazetto <gabriel@gitlab.com>
2016-12-17 00:18:16 +0100

    Warn in `gitlab-shell` that `.ssh` directory is managed by ssh_keygen too

--- files/gitlab-cookbooks/gitlab/recipes/gitlab-shell.rb
+++ files/gitlab-cookbooks/gitlab/recipes/gitlab-shell.rb
@@ -43,6 +43,9 @@ repositories_storages.each do |_name, repositories_storage|
   end
 end
 
+# Creates `.ssh` directory to hold authorized_keys
+# IMPORTANT: the `.ssh` directory is also managed by the `ssh_keygen` resource
+# in the EE package.
 [
   ssh_dir,
   File.dirname(authorized_keys)
