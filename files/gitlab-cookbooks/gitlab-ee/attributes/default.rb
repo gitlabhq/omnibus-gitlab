a4f650a8fc4ee23b6a80df4aed91fb26f0562fbd
DJ Mountney <dj@gitlab.com>
2016-12-20 23:25:53 -0800

    Fix attribute name for sidekiq-cluster

--- files/gitlab-cookbooks/gitlab-ee/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab-ee/attributes/default.rb
@@ -33,4 +33,4 @@ default['gitlab']['sidekiq-cluster']['enable'] = false
 default['gitlab']['sidekiq-cluster']['ha'] = false
 default['gitlab']['sidekiq-cluster']['log_directory'] = "/var/log/gitlab/sidekiq-cluster"
 default['gitlab']['sidekiq-cluster']['interval'] = nil
-default['gitlab']['sidekiq-cluster']['queues'] = []
+default['gitlab']['sidekiq-cluster']['queue_groups'] = []
