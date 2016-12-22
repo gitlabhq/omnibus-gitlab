825309116736442c2262333b1dbf190c20b5fc20
DJ Mountney <dj@gitlab.com>
2016-12-22 14:57:05 -0800

    Add healthcheck binary to the package
    This will allow us to configure the healthcheck curl request
    we make for containers from within the omnibus package.
    
    Otherwise it may not be clear with port/url to reach the site on

---
+++ config/software/gitlab-healthcheck.rb
@@ -0,0 +1,58 @@
+#
+# Copyright:: Copyright (c) 2016 GitLab Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "gitlab-healthcheck"
+
+license "Apache-2.0"
+license_file File.expand_path("LICENSE", Omnibus::Config.project_root)
+# This 'software' is self-contained in this file. Use the file contents
+# to generate a version string.
+default_version Digest::MD5.file(__FILE__).hexdigest
+
+build do
+  block do
+    open("#{install_dir}/bin/gitlab-healthcheck", "w") do |file|
+      file.print <<-EOH
+#!/bin/sh
+
+error_echo()
+{
+  echo "$1" 2>& 1
+}
+
+gitlab_healthcheck_rc='/opt/gitlab/etc/gitlab-healthcheck-rc'
+
+
+if ! [ -f ${gitlab_healthcheck_rc} ] ; then
+  exit 1
+fi
+
+. ${gitlab_healthcheck_rc}
+
+if [ "$(id -n -u)" = "${psql_user}" ] ; then
+  privilege_drop=''
+else
+  privilege_drop="-u ${psql_user}"
+fi
+
+exec /opt/gitlab/embedded/bin/curl $@ ${host}:${port}${path}
+       EOH
+    end
+  end
+
+  command "chmod 755 #{install_dir}/bin/gitlab-healthcheck"
+end
--- files/gitlab-cookbooks/gitlab/recipes/default.rb
+++ files/gitlab-cookbooks/gitlab/recipes/default.rb
@@ -133,6 +133,9 @@ include_recipe "gitlab::logrotate_folders_and_configs"
   end
 end
 
+# Configure healthcheck if we have the external_url set
+include_recipe "gitlab::gitlab-healthcheck" if node['gitlab']['external_url']
+
 # Recipe which handles all prometheus related services
 include_recipe "gitlab::gitlab-prometheus"
 
