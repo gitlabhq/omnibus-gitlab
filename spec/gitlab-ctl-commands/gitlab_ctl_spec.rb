981d4d7633759127eda1f3cfbb1b27a3551ab06c
Ian Baum <ibaum@gitlab.com>
2017-01-26 14:36:40 -0600

    Fixing test of omnibus-ctl commands
    The command that knapsack creates to execute tests ends up passing
    the ARGV for rspec to the underlying command, which is invalid.

--- spec/gitlab-ctl-commands/gitlab_ctl_spec.rb
+++ spec/gitlab-ctl-commands/gitlab_ctl_spec.rb
@@ -8,9 +8,14 @@ describe 'gitlab-ctl' do
   end
 
   it 'should be able to load all files' do
+    # ARGV contains the commands that were passed to rspec, which are
+    # invalid for the omnibus-ctl commands
+    oldargv = ARGV
+    ARGV = []
     expect do
       ctl = Omnibus::Ctl.new('testing-ctl')
       ctl.load_files(@ctl_dir)
     end.to_not raise_error
+    ARGV = oldargv
   end
 end
