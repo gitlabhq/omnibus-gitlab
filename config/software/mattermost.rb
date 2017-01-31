30c944a3b211d14f0c6e96dca95f92a6c0db0426
Lindsay <lf.brock@spinpunch.com>
2017-01-31 21:35:57 +0000

    Update mattermost.rb

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -17,10 +17,10 @@
 #
 
 name "mattermost"
-default_version "3.6.1"
+default_version "3.6.2"
 
 source url: "https://releases.mattermost.com/#{version}/mattermost-team-#{version}-linux-amd64.tar.gz",
-       md5: '056f4a35cf827631f74dda8c4dfdf8ec'
+       md5: 'e54073ddb246a3865645c1a5cfe08883'
 
 relative_path "mattermost"
 
