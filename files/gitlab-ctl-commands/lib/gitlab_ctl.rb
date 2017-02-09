2281b1d07b09f44fbb935a22be957e4d95152b9e
Marin Jankovski <maxlazio@gmail.com>
2017-02-09 11:34:07 +0100

    Add empty lines.

--- files/gitlab-ctl-commands/lib/gitlab_ctl.rb
+++ files/gitlab-ctl-commands/lib/gitlab_ctl.rb
@@ -1,9 +1,11 @@
 require_relative 'gitlab_ctl/pg_upgrade'
 require_relative 'gitlab_ctl/util'
+
 module GitlabCtl
   class Errors
     class ExecutionError < StandardError
       attr_accessor :command, :stdout, :stderr
+
       def initialize(command, stdout, stderr)
         @command = command
         @stdout = stdout
