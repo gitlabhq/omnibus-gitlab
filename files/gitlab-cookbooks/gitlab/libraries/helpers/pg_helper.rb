88683884e769fe8404acf0f93524742f27dc3d77
Marin Jankovski <marin@gitlab.com>
2017-03-08 13:18:43 +0000

    Merge branch '1977-geo-postgres-tracking-database' into 'master'
    Geo PostgreSQL tracking database
    
    Closes #1977
    
    See merge request !1346

---
+++ files/gitlab-cookbooks/gitlab/libraries/helpers/pg_helper.rb
@@ -0,0 +1,16 @@
+require_relative 'base_pg_helper'
+
+# Helper class to interact with bundled PostgreSQL instance
+class PgHelper < BasePgHelper
+  protected
+
+  # internal name for the service (node['gitlab'][service_name])
+  def service_name
+    'postgresql'
+  end
+
+  # command wrapper name
+  def service_cmd
+    'gitlab-psql'
+  end
+end
