da831358971bbaafee3562d4b01a19733cb9e4db
Ian Baum <ibaum@gitlab.com>
2017-03-23 17:57:29 -0700

    Move call to pg-upgrade until after the migrations have been run
    Right now its being called with some pending migrations. If the
    migrations succeed, but something else fails then the revert is to a
    version of the data without the migrations applied.

--- files/gitlab-ctl-commands/upgrade.rb
+++ files/gitlab-ctl-commands/upgrade.rb
@@ -44,15 +44,6 @@ add_command 'upgrade', 'Run migrations after a package upgrade', 1 do |cmd_name|
     exit! 0
   end
 
-  unless progress_message('Ensuring PostgreSQL is updated') do
-    command = %W(#{base_path}/bin/gitlab-ctl pg-upgrade -w)
-    status = run_command(command.join(' '))
-    status.success?
-  end
-    log 'Error ensuring PostgreSQL is updated. Please check the logs'
-    exit! 1
-  end
-
   log 'Shutting down all GitLab services except those needed for migrations'
   get_all_services.each do |sv_name|
     run_sv_command_for_service('stop', sv_name)
@@ -106,6 +97,15 @@ add_command 'upgrade', 'Run migrations after a package upgrade', 1 do |cmd_name|
     if /^run: #{sv_name}:/.match(service_statuses)
       run_sv_command_for_service('start', sv_name)
     end
+  end
+
+  unless progress_message('Ensuring PostgreSQL is updated') do
+    command = %W(#{base_path}/bin/gitlab-ctl pg-upgrade -w)
+    status = run_command(command.join(' '))
+    status.success?
+  end
+    log 'Error ensuring PostgreSQL is updated. Please check the logs'
+    exit! 1
   end
 
   log <<EOS
