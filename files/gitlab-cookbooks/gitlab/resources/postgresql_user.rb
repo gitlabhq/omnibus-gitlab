9ff10453e294f1cd6349abb79de45566c1fce53c
Marin Jankovski <marin@gitlab.com>
2017-05-18 10:45:41 +0000

    Merge branch '2348-postgresql_user-does-not-work-with-geo-postgresql-recipe' into 'master'
    Update postgresql_user resource to allow different helpers
    
    Closes #2348
    
    See merge request !1561

--- files/gitlab-cookbooks/gitlab/resources/postgresql_user.rb
+++ files/gitlab-cookbooks/gitlab/resources/postgresql_user.rb
@@ -2,10 +2,10 @@ resource_name :postgresql_user
 
 property :username, String, name_property: true
 property :password, String
+property :helper, default: PgHelper.new(node)
 
 action :create do
   account_helper = AccountHelper.new(node)
-  pg_helper = PgHelper.new(node)
 
   query = if password.nil?
             "CREATE USER #{username};"
@@ -13,10 +13,10 @@ action :create do
             "CREATE USER #{username} PASSWORD '#{password}';"
           end
   execute "create #{username} postgresql user" do
-    command %(/opt/gitlab/bin/gitlab-psql -d template1 -c "#{query}")
+    command %(/opt/gitlab/bin/#{helper.service_cmd} -d template1 -c "#{query}")
     user account_helper.postgresql_user
     # Added retries to give the service time to start on slower systems
     retries 20
-    not_if { !pg_helper.is_running? || pg_helper.user_exists?(username) }
+    not_if { !helper.is_running? || helper.user_exists?(username) }
   end
 end
