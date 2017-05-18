40434a9661c35e8039fdb576d4d8cbf969e2486c
DJ Mountney <dj@gitlab.com>
2017-05-17 20:28:44 -0700

    Use a forked registry that includes an updated version of aws-sdk-go
    This should fix issues with pushing to registries backed by s3 storage

--- config/software/registry.rb
+++ config/software/registry.rb
@@ -19,12 +19,12 @@
 require "#{Omnibus::Config.project_root}/lib/gitlab/version"
 
 name 'registry'
-version = Gitlab::Version.new('registry', '2.6.1')
+version = Gitlab::Version.new('registry', 'dd544a82d93e8d39cec3d3d45117c3f486365589')
 
 default_version version.print
 
 license 'Apache-2.0'
-license_file "https://raw.githubusercontent.com/docker/distribution/#{version.print}/LICENSE"
+license_file "https://gitlab.com/twk3/docker-distribution/raw/#{version.print}/LICENSE"
 
 source git: version.remote
 
