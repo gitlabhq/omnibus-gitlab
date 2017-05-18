6fdfe5aacb683ceb2e78767a82b03684d6feeddd
Balasankar C <balasankar@gitlab.com>
2017-05-18 23:07:34 +0530

    Run rubocop over support directory

--- support/triggered_package.rb
+++ support/triggered_package.rb
@@ -2,13 +2,12 @@
 require 'net/http'
 require 'json'
 
-
-uri=URI("https://gitlab.com/api/v4/projects/#{ENV['CI_PROJECT_ID']}/pipelines/#{ENV['CI_PIPELINE_ID']}/jobs")
+uri = URI("https://gitlab.com/api/v4/projects/#{ENV['CI_PROJECT_ID']}/pipelines/#{ENV['CI_PIPELINE_ID']}/jobs")
 req = Net::HTTP::Get.new(uri)
 req['PRIVATE-TOKEN'] = ENV["TRIGGER_PRIVATE_TOKEN"]
 http = Net::HTTP.new(uri.hostname, uri.port)
-http.use_ssl=true
+http.use_ssl = true
 res = http.request(req)
 output = JSON.parse(res.body)
-id = output.find {|job| job['name'] == 'Trigger:package'}['id']
+id = output.find { |job| job['name'] == 'Trigger:package' }['id']
 puts "#{ENV['CI_PROJECT_URL']}/builds/#{id}/artifacts/file/pkg/ubuntu-16.04/gitlab.deb"
