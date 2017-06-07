05a0f6015b2984bd31d51eec1ba651439bbe93af
Ben Kochie <bjk@gitlab.com>
2017-06-07 15:20:16 +0200

    Update Prometheus version
    * Update from 1.6.1 to 1.6.3 (minor bug fixes).

--- config/software/prometheus.rb
+++ config/software/prometheus.rb
@@ -19,7 +19,7 @@
 require "#{Omnibus::Config.project_root}/lib/gitlab/version"
 
 name 'prometheus'
-version = Gitlab::Version.new('prometheus', '1.6.1')
+version = Gitlab::Version.new('prometheus', '1.6.3')
 default_version version.print
 
 license 'APACHE-2.0'
