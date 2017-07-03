c45d17dac6162125e9700f9be12716bc7058d6b0
Jacob Vosmaer <jacob@gitlab.com>
2017-07-03 14:21:40 +0200

    Remove code we no longer need

--- files/gitlab-cookbooks/gitlab/libraries/gitaly.rb
+++ files/gitlab-cookbooks/gitlab/libraries/gitaly.rb
@@ -17,28 +17,6 @@
 
 module Gitaly
   class << self
-    def parse_variables
-      parse_gitaly_environment_variables
-    end
-
-    def parse_gitaly_environment_variables
-      gitaly_env = Gitlab['node']['gitlab']['gitaly']['env'].to_hash
-      gitlab_rb_keys = Gitlab['gitaly'].keys
-      gitaly_env.merge!(Gitlab['gitaly']['env']) if Gitlab['gitaly']['env']
-
-      if gitlab_rb_keys
-        # Find any user-set keys that don't exist in our attributes and set them
-        # as environment variables for the gitaly executable.
-        # This is so the gitaly executable config requirements can change independant of omnibus.
-        # For example: Setting gitaly['socket_path'] with set GITALY_SOCKET_PATH
-        (gitlab_rb_keys - Gitlab['node']['gitlab']['gitaly'].keys).each do |key|
-          gitaly_env["GITALY_#{key.upcase}"] = "#{Gitlab['gitaly'][key]}"
-        end
-      end
-
-      Gitlab['gitaly']['env'] = gitaly_env
-    end
-
     def gitaly_address
       socket_path = user_config['socket_path'] || package_default['socket_path']
       listen_addr = user_config['listen_addr'] || package_default['listen_addr']
--- spec/libraries/gitaly_spec.rb
+++ spec/libraries/gitaly_spec.rb
@@ -16,24 +16,4 @@ describe Gitaly do
       expect(chef_run.node['gitlab']['gitaly']['env']).not_to include('GITALY_ENABLE')
     end
   end
-
-  describe 'when unknown gitaly setting and new env is provided' do
-    before { stub_gitlab_rb(gitaly: { cool_feature: true, env: { 'TEST' => 'true' } }) }
-
-    it 'puts the setting into the environment and maintains other environment settings' do
-      expect(chef_run.node['gitlab']['gitaly']['env']).to include('GITALY_COOL_FEATURE' => 'true', 'TEST' => 'true')
-    end
-
-    it 'does not include known settings in the environment' do
-      expect(chef_run.node['gitlab']['gitaly']['env']).not_to include('GITALY_ENABLE')
-    end
-  end
-
-  describe 'when unkown gitaly setting is provided' do
-    before { stub_gitlab_rb(gitaly: { cool_feature: true }) }
-
-    it 'puts the setting into the environment and maintians other environment settings' do
-      expect(chef_run.node['gitlab']['gitaly']['env']).to include({ 'GITALY_COOL_FEATURE' => 'true' })
-    end
-  end
 end
