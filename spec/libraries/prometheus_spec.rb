2b8e9cebe27d602d8f681aa835e404df3bbe0296
DJ Mountney <dj@gitlab.com>
2017-07-08 16:02:18 -0700

    Fix rubocop indentation

--- spec/libraries/prometheus_spec.rb
+++ spec/libraries/prometheus_spec.rb
@@ -3,11 +3,11 @@ require 'chef_helper'
 describe Prometheus do
   it 'should return a list of known services' do
     expect(Prometheus.services).to match_array(%w(
-                                        prometheus
-                                        node-exporter
-                                        redis-exporter
-                                        postgres-exporter
-                                        gitlab-monitor
-                                      ))
+                                                 prometheus
+                                                 node-exporter
+                                                 redis-exporter
+                                                 postgres-exporter
+                                                 gitlab-monitor
+                                               ))
   end
 end
