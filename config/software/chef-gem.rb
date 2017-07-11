c2bd5ff6373677d316bf5c9ffbabde12d39b4c7c
Marin Jankovski <marin@gitlab.com>
2017-07-11 10:40:27 +0000

    Merge branch '2568-license-generation-issues' into 'master'
    Resolve "License generation issues"
    
    Closes #2568
    
    See merge request !1731

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -18,7 +18,7 @@ name 'chef-gem'
 default_version '12.12.15'
 
 license 'Apache-2.0'
-license_file 'https://github.com/chef/chef/blob/master/LICENSE'
+license_file 'https://github.com/chef/chef/raw/master/LICENSE'
 
 dependency 'ruby'
 dependency 'rubygems'
