de24909882a1b953978565e2d44d4b133505872a
DJ Mountney <dj@gitlab.com>
2017-07-12 16:34:26 -0700

    Load services for the prometheus library test

--- spec/libraries/prometheus_spec.rb
+++ spec/libraries/prometheus_spec.rb
@@ -1,6 +1,8 @@
 require 'chef_helper'
 
 describe Prometheus do
+  before { Services.add_services('gitlab', Gitlab::Services.list) }
+
   it 'should return a list of known services' do
     expect(Prometheus.services).to match_array(%w(
                                                  prometheus
