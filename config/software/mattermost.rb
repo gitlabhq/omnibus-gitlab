ce50d5f8541b5cc114dfcdfbf6ac08468c579cf9
Brian Neel <brian@gitlab.com>
2017-07-18 14:20:42 -0400

    Update mattermost to 3.7.6

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -17,10 +17,10 @@
 #
 
 name 'mattermost'
-default_version '3.7.5'
+default_version '3.7.6'
 
 source url: "https://releases.mattermost.com/#{version}/mattermost-team-#{version}-linux-amd64.tar.gz",
-       md5: '94fb39daca94a506cd188afba2102146'
+       md5: '3447bfc35c490cd311a9bf671662418b'
 
 relative_path 'mattermost'
 
