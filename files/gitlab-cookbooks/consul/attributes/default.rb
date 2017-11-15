0fb31caa83d309303332fe7477e8fe625eab4d9c
Marin Jankovski <marin@gitlab.com>
2017-11-15 18:23:09 +0000

    Merge branch 'jt/make-consul-node-name-configurable' into 'master'
    Jt/make consul node name configurable
    
    See merge request gitlab-org/omnibus-gitlab!2076

--- files/gitlab-cookbooks/consul/attributes/default.rb
+++ files/gitlab-cookbooks/consul/attributes/default.rb
@@ -5,6 +5,7 @@ default['consul']['config_file'] = '/var/opt/gitlab/consul/config.json'
 default['consul']['config_dir'] = '/var/opt/gitlab/consul/config.d'
 default['consul']['data_dir'] = '/var/opt/gitlab/consul/data'
 default['consul']['log_directory'] = '/var/log/gitlab/consul'
+default['consul']['node_name'] = nil
 default['consul']['script_directory'] = '/var/opt/gitlab/consul/scripts'
 default['consul']['configuration'] = {}
 
