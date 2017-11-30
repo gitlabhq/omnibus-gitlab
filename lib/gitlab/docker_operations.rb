37f457950973b3f72cd142342e8b44ac03c616c3
Rémy Coutable <remy@rymai.me>
2017-11-30 17:24:55 +0100

    Add specs for DockerOperations
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/gitlab/docker_operations.rb
+++ lib/gitlab/docker_operations.rb
@@ -16,23 +16,7 @@ class DockerOperations
   end
 
   def self.authenticate(username = ENV['DOCKERHUB_USERNAME'], password = ENV['DOCKERHUB_PASSWORD'], serveraddress = "")
-    Docker.authenticate!(username: username, password: password, serveraddress: registry)
-  end
-
-  # namespace - registry project. Can be one of:
-  # 1. gitlab/gitlab-{ce,ee}
-  # 2. gitlab/gitlab-{ce,ee}-qa
-  # 3. omnibus-gitlab/gitlab-{ce,ee}
-  #
-  # initial_tag - specifies the tag used while building the image. Can be one of:
-  # 1. latest - for GitLab images
-  # 2. ce-latest or ee-latest - for GitLab QA images
-  # 3. any other valid docker tag
-  #
-  # new_tag - specifies the new tag for the existing image
-  def self.tag_and_push(initial_namespace, new_namespace, initial_tag, new_tag)
-    tag(initial_namespace, new_namespace, initial_tag, new_tag)
-    push(new_namespace, new_tag)
+    Docker.authenticate!(username: username, password: password, serveraddress: serveraddress)
   end
 
   def self.get(namespace, tag)
@@ -52,5 +36,21 @@ class DockerOperations
     set_timeout
     image = get(initial_namespace, initial_tag)
     image.tag(repo: new_namespace, tag: new_tag, force: true)
+  end
+
+  # namespace - registry project. Can be one of:
+  # 1. gitlab/gitlab-{ce,ee}
+  # 2. gitlab/gitlab-{ce,ee}-qa
+  # 3. omnibus-gitlab/gitlab-{ce,ee}
+  #
+  # initial_tag - specifies the tag used while building the image. Can be one of:
+  # 1. latest - for GitLab images
+  # 2. ce-latest or ee-latest - for GitLab QA images
+  # 3. any other valid docker tag
+  #
+  # new_tag - specifies the new tag for the existing image
+  def self.tag_and_push(initial_namespace, new_namespace, initial_tag, new_tag)
+    tag(initial_namespace, new_namespace, initial_tag, new_tag)
+    push(new_namespace, new_tag)
   end
 end
--- spec/gitlab/build/qa_image_spec.rb
+++ spec/gitlab/build/qa_image_spec.rb
@@ -3,10 +3,8 @@ require 'chef_helper'
 
 describe Build::QAImage do
   before do
-    allow(Build::Image).to receive(:dockerhub_image_name).and_return('gitlab/gitlab-ce')
-    allow(Build::Image).to receive(:gitlab_registry_image_name).and_return('gitlab-ce')
-    allow(ENV).to receive(:[]).with('CI_REGISTRY').and_return('registry.com')
-    allow(ENV).to receive(:[]).with('CI_REGISTRY_IMAGE').and_return('registry.com/group/repo')
+    allow(Build::GitlabImage).to receive(:dockerhub_image_name).and_return('gitlab/gitlab-ce')
+    allow(Build::GitlabImage).to receive(:gitlab_registry_image_name).and_return('gitlab-ce')
   end
 
   describe '.dockerhub_image_name' do
