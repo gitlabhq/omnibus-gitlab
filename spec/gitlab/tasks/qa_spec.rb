3411325d5b81b0e667f7008eaae41e875428c7c4
Balasankar C <balasankar@gitlab.com>
2018-01-10 13:02:03 +0000

    Make trigger/polling code general and use it for triggers from omnibus

--- spec/gitlab/tasks/qa_spec.rb
+++ spec/gitlab/tasks/qa_spec.rb
@@ -75,8 +75,17 @@ describe 'qa', type: :rake do
   end
 
   describe 'qa:test' do
+    class FakeResponse
+      attr_reader :body
+      def initialize
+        @body = "{\"id\": \"1\"}"
+      end
+    end
+
     let(:qatrigger) { Build::QATrigger.new }
-    let(:qapipeline) { Build::QAPipeline.new(1) }
+    let(:qapipeline) { Build::Trigger::Pipeline.new(1, "gitlab-org/gitlab-qa", "asdf12345") }
+    let(:qaresponse) { FakeResponse.new }
+
     before do
       Rake::Task['qa:build'].reenable
       Rake::Task['qa:push:triggered'].reenable
@@ -93,75 +102,73 @@ describe 'qa', type: :rake do
       allow(Build::GitlabImage).to receive(:tag_and_push_to_gitlab_registry).and_return(true)
       allow(Build::QAImage).to receive(:gitlab_registry_image_address).and_return(gitlab_registry_image_address)
       allow(Build::QAImage).to receive(:tag_and_push_to_gitlab_registry).and_return(true)
-      allow(Build::QATrigger).to receive(:new).and_return(qatrigger)
-      allow_any_instance_of(Build::QAPipeline).to receive(:timeout?).and_return(false)
-      allow_any_instance_of(Build::QATrigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:timeout?).and_return(false)
     end
 
     it 'triggers QA pipeline correcty' do
-      class FakeResponse
-        attr_reader :body
-        def initialize
-          @body = "{\"id\": \"1\"}"
-        end
-      end
-      allow(Net::HTTP).to receive(:post_form).and_return(FakeResponse.new)
-      allow_any_instance_of(Build::QATrigger).to receive(:invoke!).and_call_original
-      allow(Build::QATrigger).to receive(:new).and_call_original
-      stub_const("Build::QATrigger::TOKEN", "1234")
-      allow_any_instance_of(Build::QAPipeline).to receive(:status).and_return(:success)
-
-      uri = URI("https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab-qa/trigger/pipeline")
-      params = {
+      api_uri = URI("https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab-qa/trigger/pipeline")
+      api_params = {
         "ref" => "master",
         "token" => "1234",
         "variables[RELEASE]" => "registry.gitlab.com/gitlab-ce:latest",
         "variables[TRIGGERED_USER]" => "John Doe",
         "variables[TRIGGER_SOURCE]" => "https://gitlab.com/gitlab-org/omnibus-gitlab/-/jobs/12345"
       }
-      expect(Net::HTTP).to receive(:post_form).with(uri, params)
+      allow_any_instance_of(Build::QATrigger).to receive(:invoke!).and_call_original
+      allow_any_instance_of(Build::QATrigger).to receive(:get_projct_path).and_return("gitlab-org/gitlab-qa")
+      allow_any_instance_of(Build::QATrigger).to receive(:get_access_token).and_return("1234")
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:status).and_return(:success)
+
+      allow(Net::HTTP).to receive(:post_form).with(api_uri, api_params).and_return(qaresponse)
+      expect(Net::HTTP).to receive(:post_form).with(api_uri, api_params)
       Rake::Task['qa:test'].invoke
     end
 
     it 'detects created pipeline' do
-      allow_any_instance_of(Build::QAPipeline).to receive(:status).and_return(:created, :success)
+      allow_any_instance_of(Build::Trigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:status).and_return(:created, :success)
 
-      expect_any_instance_of(Build::QAPipeline).to receive(:sleep)
+      expect_any_instance_of(Build::Trigger::Pipeline).to receive(:sleep)
       Rake::Task['qa:test'].invoke
       expect { Rake::Task['qa:test'].invoke }.not_to raise_error
     end
 
     it 'detects pending pipeline' do
-      allow_any_instance_of(Build::QAPipeline).to receive(:status).and_return(:pending, :success)
+      allow_any_instance_of(Build::Trigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:status).and_return(:pending, :success)
 
-      expect_any_instance_of(Build::QAPipeline).to receive(:sleep)
+      expect_any_instance_of(Build::Trigger::Pipeline).to receive(:sleep)
       Rake::Task['qa:test'].invoke
       expect { Rake::Task['qa:test'].invoke }.not_to raise_error
     end
 
     it 'detects running pipeline' do
-      allow_any_instance_of(Build::QAPipeline).to receive(:status).and_return(:running, :success)
+      allow_any_instance_of(Build::Trigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:status).and_return(:running, :success)
 
-      expect_any_instance_of(Build::QAPipeline).to receive(:sleep)
+      expect_any_instance_of(Build::Trigger::Pipeline).to receive(:sleep)
       Rake::Task['qa:test'].invoke
       expect { Rake::Task['qa:test'].invoke }.not_to raise_error
     end
 
     it 'detects successful pipeline' do
-      allow_any_instance_of(Build::QAPipeline).to receive(:status).and_return(:success)
+      allow_any_instance_of(Build::Trigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:status).and_return(:success)
 
       expect { Rake::Task['qa:test'].invoke }.not_to raise_error
     end
 
     it 'detects failed pipeline' do
-      allow_any_instance_of(Build::QAPipeline).to receive(:status).and_return(:failed)
+      allow_any_instance_of(Build::Trigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:status).and_return(:failed)
 
-      expect { Rake::Task['qa:test'].invoke }.to raise_error(RuntimeError, "QA pipeline did not succeed!")
+      expect { Rake::Task['qa:test'].invoke }.to raise_error(RuntimeError, "Pipeline did not succeed!")
     end
 
     it 'times out correctly' do
-      allow_any_instance_of(Build::QAPipeline).to receive(:timeout?).and_return(true)
-      allow_any_instance_of(Build::QAPipeline).to receive(:duration).and_return(10)
+      allow_any_instance_of(Build::Trigger).to receive(:invoke!).and_return(qapipeline)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:timeout?).and_return(true)
+      allow_any_instance_of(Build::Trigger::Pipeline).to receive(:duration).and_return(10)
 
       expect { Rake::Task['qa:test'].invoke }.to raise_error(RuntimeError, "Pipeline timed out after waiting for 10 minutes!")
     end
