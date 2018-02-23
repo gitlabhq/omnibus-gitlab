38dc7921a4e702b6ce9882726e3098d39fd8b731
Harrison Healey <harrisonmhealey@gmail.com>
2018-02-23 09:47:41 -0500

    Updated Mattermost to 4.7.2

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -17,10 +17,10 @@
 #
 
 name 'mattermost'
-default_version '4.6.0'
+default_version '4.7.2'
 
 source url: "https://releases.mattermost.com/#{version}/mattermost-team-#{version}-linux-amd64.tar.gz",
-       md5: '24c787aff8d9222409c3584b9638aaac'
+       md5: '9cc729efded3ae32a92640821fe3f46e'
 
 relative_path 'mattermost'
 
