5288ec53a22075fe1095facc0eb9a1cf44db12b8
Stan Hu <stanhu@gmail.com>
2018-02-26 12:31:37 -0800

    Bump git from 2.14.3 to 2.16.2
    git 2.15.0 has some significant performance improvements when running `git
    fetch` on a repository that has lots of references. See
    https://gitlab.com/gitlab-com/migration/issues/216#note_60672987 for more
    details.
    
    The issue with stale worktrees we had bumping up to git 2.16 should be
    resolved via https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/18095/diffs.
    
    Closes #3375

--- config/software/git.rb
+++ config/software/git.rb
@@ -26,7 +26,7 @@ name 'git'
 # - https://gitlab.com/gitlab-org/gitlab-development-kit/blob/master/doc/prepare.md
 # - https://gitlab.com/gitlab-org/gitlab-build-images/blob/master/.gitlab-ci.yml
 # - https://gitlab.com/gitlab-org/gitlab-ce/blob/master/.gitlab-ci.yml
-default_version '2.14.3'
+default_version '2.16.2'
 
 license 'GPL-2.0'
 license_file 'COPYING'
@@ -37,7 +37,7 @@ dependency 'openssl'
 dependency 'curl'
 
 source url: "https://www.kernel.org/pub/software/scm/git/git-#{version}.tar.gz",
-       sha256: '023ffff6d3ba8a1bea779dfecc0ed0bb4ad68ab8601d14435dd8c08416f78d7f'
+       sha256: '9acc4339b7a2ab484eea69d705923271682b7058015219cf5a7e6ed8dee5b5fb'
 
 relative_path "git-#{version}"
 
