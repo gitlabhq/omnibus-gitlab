1a54f2c0a6a85b1d091635ff0e0be77ecb429151
Gabriel Mazetto <gabriel@gitlab.com>
2018-03-14 06:31:33 +0100

    Change gitlab.rb template and remove useless default action

--- files/gitlab-cookbooks/gitlab-ee/recipes/geo-postgresql.rb
+++ files/gitlab-cookbooks/gitlab-ee/recipes/geo-postgresql.rb
@@ -170,7 +170,6 @@ if node['gitlab']['geo-postgresql']['enable']
     query "CREATE SCHEMA gitlab_secondary;"
     db_name geo_database_name
     helper geo_pg_helper
-    action :run
 
     not_if { !fdw_helper.fdw_enabled? || geo_pg_helper.is_offline_or_readonly? || geo_pg_helper.schema_exists?('gitlab_secondary', geo_database_name) }
   end
