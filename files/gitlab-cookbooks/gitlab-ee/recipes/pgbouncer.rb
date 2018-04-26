3b1fcd2d4c0f750d7193a507c9a2630df6acea7e
Ian Baum <ibaum@gitlab.com>
2018-04-26 15:18:15 -0500

    Update pgbouncer recipes default databases.ini user
    Use the pgbouncer user by default

--- files/gitlab-cookbooks/gitlab-ee/recipes/pgbouncer.rb
+++ files/gitlab-cookbooks/gitlab-ee/recipes/pgbouncer.rb
@@ -63,6 +63,8 @@ file 'databases.json' do
   notifies :run, 'execute[generate databases.ini]', :immediately
 end
 
+ini_user = node['gitlab']['postgresql']['pgbouncer_user'] || node['gitlab']['pgbouncer']['databases_ini_user']
+
 execute 'generate databases.ini' do
   command lazy {
     <<~EOF
@@ -72,7 +74,7 @@ execute 'generate databases.ini' do
      --hostuser #{node['gitlab']['pgbouncer']['databases_ini_user']} \
      --pg-host #{node['gitlab']['pgbouncer']['listen_addr']} \
      --pg-port #{node['gitlab']['pgbouncer']['listen_port']} \
-     --user #{node['gitlab']['pgbouncer']['databases_ini_user']}
+     --user #{ini_user}
     EOF
   }
   action :nothing
