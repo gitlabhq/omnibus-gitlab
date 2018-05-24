52f764e511225316cd306f9df3ab88a6237092d9
Balasankar C <balasankar@gitlab.com>
2018-05-24 09:07:03 +0000

    Remove support for deprecated git_data_dir setting structure

--- files/gitlab-cookbooks/gitaly/libraries/gitaly.rb
+++ files/gitlab-cookbooks/gitaly/libraries/gitaly.rb
@@ -25,7 +25,6 @@ module Gitaly
     def parse_variables
       parse_git_data_dirs
       parse_gitaly_storages
-      detect_deprecated_settings
     end
 
     def gitaly_address
@@ -40,58 +39,11 @@ module Gitaly
       end
     end
 
-    def detect_deprecated_settings
-      git_data_dirs = Gitlab['git_data_dirs']
-      deprecated_key_used = 'git_data_dir' if Gitlab['git_data_dir']
-      if git_data_dirs.any?
-        git_data_dirs.map do |name, data_directory|
-          if data_directory.is_a?(String)
-            deprecated_key_used = 'git_data_dirs'
-            break
-          end
-        end
-      end
-
-      if deprecated_key_used # rubocop:disable Style/GuardClause
-        warn_message = <<~EOS
-          Your #{deprecated_key_used} settings are deprecated.
-          Please update it to the following:
-
-          git_data_dirs(#{print_ruby_object(converted_git_data_dirs)})
-
-          Please refer to https://docs.gitlab.com/omnibus/settings/configuration.html#storing-git-data-in-an-alternative-directory for updated documentation.
-        EOS
-        LoggingHelper.deprecation warn_message
-      end
-    end
-
-    def converted_git_data_dirs
-      # Converting values in old formats to the correct, new one.
-      # Before version 8.10 we used git_data_dir configuration, which had a string representing a path as value.
-      # From version 8.10 till version 8.17.8, we used git_data_dirs configuration, which had a { <name> => <path> } hash as value.
-      # Now, since version 9.0, git_data_dirs has a { <name> => {"path" => <path> } } hash as value.
-      # So we convert all the old formats to the new one, until we remove the support of them.
-
-      git_data_dirs = Gitlab['git_data_dirs']
-      git_data_dir = Gitlab['git_data_dir']
-      return { "default" => { "path" => "/var/opt/gitlab/git-data" } } unless git_data_dirs.any? || git_data_dir
-
-      if git_data_dirs.any?
-        Mash.new(Hash[git_data_dirs.map do |name, data_directory|
-          if data_directory.is_a?(String)
-            [name, { 'path' => data_directory }]
-          else
-            [name, data_directory]
-          end
-        end])
-      else
-        { 'default' => { 'path' => git_data_dir } }
-      end
-    end
-
     def parse_git_data_dirs
+      Gitlab['git_data_dirs'] = { "default" => { "path" => "/var/opt/gitlab/git-data" } } if Gitlab['git_data_dirs'].empty?
+
       Gitlab['gitlab_rails']['repositories_storages'] =
-        Hash[converted_git_data_dirs.map do |name, data_directory|
+        Hash[Mash.new(Gitlab['git_data_dirs']).map do |name, data_directory|
           shard_gitaly_address = data_directory['gitaly_address'] || gitaly_address
 
           defaults = { 'path' => File.join(data_directory['path'], 'repositories'), 'gitaly_address' => shard_gitaly_address }
