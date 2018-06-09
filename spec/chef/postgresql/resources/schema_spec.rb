965c2c568117158755313a8a77108cf138fdf469
Gabriel Mazetto <gabriel@gitlab.com>
2018-06-09 19:02:30 +0200

    Added specs to postgresql schema creation

---
+++ spec/chef/postgresql/resources/schema_spec.rb
@@ -0,0 +1,52 @@
+require 'chef_helper'
+
+describe 'postgresql_schema' do
+  let(:runner) { ChefSpec::SoloRunner.new(step_into: %w(postgresql_schema)) }
+
+  context 'create' do
+    let(:chef_run) { runner.converge('test_gitlab_postgresql_schema::create') }
+
+    context 'server is running' do
+      before do
+        allow_any_instance_of(PgHelper).to receive(:is_running?).and_return(true)
+      end
+
+      it 'creates schema' do
+        expect(chef_run).to run_postgresql_query('create example schema on omnibus_gitlab_test').with(
+          owner: 'CURRENT_USER',
+          database: 'omnibus_gitlab_test'
+        )
+      end
+
+      context 'schema already exists' do
+        before do
+          allow_any_instance_of(PgHelper).to receive(:schema_exists?).and_return(true)
+        end
+
+        it 'does not try to create schema again' do
+          expect(chef_run).not_to run_postgresql_query('create example schema on omnibus_gitlab_test')
+        end
+
+        context 'when schema owner is different' do
+          before do
+            allow_any_instance_of(PgHelper).to receive(:schema_owner?).and_return(false)
+          end
+
+          it 'tries to update schema' do
+            expect(chef_run).to run_postgresql_query('modify example schema owner on omnibus_gitlab_test')
+          end
+        end
+
+        context 'when schema owner is the same' do
+          before do
+            allow_any_instance_of(PgHelper).to receive(:schema_owner?).and_return(true)
+          end
+
+          it 'tries to update schema' do
+            expect(chef_run).not_to run_postgresql_query('modify example schema owner on omnibus_gitlab_test')
+          end
+        end
+      end
+    end
+  end
+end
