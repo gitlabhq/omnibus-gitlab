c893ebf6643b7a08ed9d4aedaa4c5fd03723089d
Balasankar "Balu" C <balasankar@gitlab.com>
2018-06-28 20:37:23 +0530

    Use psotgresql group

--- files/gitlab-cookbooks/gitlab-ee/recipes/pgbouncer.rb
+++ files/gitlab-cookbooks/gitlab-ee/recipes/pgbouncer.rb
@@ -74,7 +74,7 @@ execute 'generate databases.ini' do
      --databases-json #{node['gitlab']['pgbouncer']['databases_json']} \
      --databases-ini #{node['gitlab']['pgbouncer']['databases_ini']} \
      --hostuser #{node['gitlab']['pgbouncer']['databases_ini_user']} \
-     --hostgroup #{node['gitlab']['postgresql']['username']} \
+     --hostgroup account_helper.postgresql_group \
      --pg-host #{node['gitlab']['pgbouncer']['listen_addr']} \
      --pg-port #{node['gitlab']['pgbouncer']['listen_port']} \
      --user #{node['gitlab']['postgresql']['pgbouncer_user']}
