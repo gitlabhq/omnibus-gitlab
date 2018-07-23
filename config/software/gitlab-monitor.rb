8b56e822837eb06c2081496f154a88504f89954f
Stan Hu <stanhu@gmail.com>
2018-07-23 10:43:32 -0700

    Bump gitlab-monitor to v2.18.0
    This removes a stale Sidekiq 4.2.10 dependency seen in
    https://gitlab.com/gitlab-org/omnibus-gitlab/issues/3698.

--- config/software/gitlab-monitor.rb
+++ config/software/gitlab-monitor.rb
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-version = Gitlab::Version.new('gitlab-monitor', '2.17.0')
+version = Gitlab::Version.new('gitlab-monitor', '2.18.0')
 
 default_version version.print
 
