3b755240413f58003068dc9186736df7c35dc2ed
Mike Greiling <mike@pixelcog.com>
2018-09-11 10:55:43 -0500

    ensure that yarn-cache is moved to the gitlab-rails source directory

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -117,7 +117,7 @@ build do
 
   # Copy asset cache and node modules from cache location to source directory
   move "#{Omnibus::Config.project_root}/assets_cache", "#{Omnibus::Config.source_dir}/gitlab-rails/tmp/cache"
-  move "#{Omnibus::Config.project_root}/node_modules", "#{Omnibus::Config.source_dir}/gitlab-rails"
+  move "#{Omnibus::Config.project_root}/.yarn-cache", "#{Omnibus::Config.source_dir}/gitlab-rails"
 
   assets_compile_env = {
     'NODE_ENV' => 'production',
@@ -144,7 +144,7 @@ build do
   # Move folders for caching. GitLab CI permits only relative path for Cache
   # and Artifacts. So we need these folder in the root directory.
   move "#{Omnibus::Config.source_dir}/gitlab-rails/tmp/cache", "#{Omnibus::Config.project_root}/assets_cache"
-  move "#{Omnibus::Config.source_dir}/gitlab-rails/node_modules", Omnibus::Config.project_root.to_s
+  move "#{Omnibus::Config.source_dir}/gitlab-rails/.yarn-cache", Omnibus::Config.project_root.to_s
 
   # Tear down now that gitlab:assets:compile is done.
   delete 'config/gitlab.yml'
