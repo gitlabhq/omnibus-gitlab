12c729f3fa59d11d2f65401efd393223b9ba5dae
Balasankar "Balu" C <balasankar@gitlab.com>
2018-09-25 18:42:58 +0530

    Set ac_cv_func_inotify_init=no flag for gnupg in Centos 6

--- config/software/gnupg.rb
+++ config/software/gnupg.rb
@@ -35,9 +35,18 @@ relative_path "gnupg-#{version}"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+  # For gnupg to build fine in Debian Wheezy and Centos ^
   env['LDFLAGS'] << " -lrt"
+
+  config_flags = ""
+  # CentOS 6 doesn't have inotify, which will raise an error
+  # IN_EXCL_UNLINK undeclared. Hence disabling it explicitly.
+  if ohai['platform'] =~ /centos/ && ohai['platform_version'] =~ /^6/
+    config_flags = "ac_cv_func_inotify_init=no"
+  end
+
   command './configure ' \
-    "--prefix=#{install_dir}/embedded --disable-doc --without-readline --disable-sqlite --disable-gnutls --disable-dirmngr", env: env
+    "--prefix=#{install_dir}/embedded --disable-doc --without-readline --disable-sqlite --disable-gnutls --disable-dirmngr #{config_flags}", env: env
 
   make "-j #{workers}", env: env
   make 'install', env: env
