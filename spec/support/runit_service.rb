08b7bcdc876135a2f4e53cb1a272587a07fb7397
Balasankar "Balu" C <balasankar@gitlab.com>
2018-10-19 11:13:30 +0530

    Update runit_service shared example to handle user and group

--- spec/support/runit_service.rb
+++ spec/support/runit_service.rb
@@ -1,4 +1,12 @@
-shared_examples 'enabled runit service' do |svc_name, owner, group|
+def get_env_string(username, groupname)
+  env_string = ""
+  env_string << username if username
+  env_string << ":#{groupname}" if groupname
+
+  env_string
+end
+
+shared_examples 'enabled runit service' do |svc_name, owner, group, username = nil, groupname = nil|
   it 'creates directories' do
     expect(chef_run).to create_directory("/opt/gitlab/sv/#{svc_name}").with(
       owner: owner,
@@ -34,7 +42,15 @@ shared_examples 'enabled runit service' do |svc_name, owner, group|
       mode: nil # 0755 is an octal value. 493 is the decimal conversion.
     )
 
-    expect(chef_run).to render_file("/opt/gitlab/sv/#{svc_name}/run").with_content(%r{.*})
+    env_string = get_env_string(username, groupname)
+
+    if env_string.empty?
+      expect(chef_run).to render_file("/opt/gitlab/sv/#{svc_name}/run").with_content(%r{.*})
+    else
+      expect(chef_run).to render_file("/opt/gitlab/sv/#{svc_name}/run").with_content(/-u #{env_string}/)
+      expect(chef_run).to render_file("/opt/gitlab/sv/#{svc_name}/run").with_content(/-U #{env_string}/)
+    end
+
     expect(chef_run).to render_file("/opt/gitlab/sv/#{svc_name}/log/run").with_content(%r{.*})
     expect(chef_run).to render_file("/var/log/gitlab/#{svc_name}/config").with_content(%r{.*})
   end
