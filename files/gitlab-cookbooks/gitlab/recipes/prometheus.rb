abfe79882e54d39ac0535c34404dc76bc3ed608e
Jason Plum <jplum@gitlab.com>
2018-10-29 16:08:23 -0400

    prometheus: add configuration attribute for environment
    Prometheus makes use of Go's crypto/tls, as such may need `SSL_CERT_DIR` defined for
    trusted certificates. Adding in the handling of the `env` node so that we can
    populate the `SSL_CERT_DIR` variable for the process.

--- files/gitlab-cookbooks/gitlab/recipes/prometheus.rb
+++ files/gitlab-cookbooks/gitlab/recipes/prometheus.rb
@@ -21,6 +21,7 @@ prometheus_user = account_helper.prometheus_user
 prometheus_log_dir = node['gitlab']['prometheus']['log_directory']
 prometheus_dir = node['gitlab']['prometheus']['home']
 prometheus_rules_dir = node['gitlab']['prometheus']['rules_directory']
+prometheus_static_etc_dir = "/opt/gitlab/etc/prometheus"
 
 binary, rule_extension = prometheus_helper.binary_and_rules
 
@@ -42,6 +43,17 @@ directory prometheus_log_dir do
   owner prometheus_user
   mode '0700'
   recursive true
+end
+
+directory prometheus_static_etc_dir do
+  owner prometheus_user
+  mode '0700'
+  recursive true
+end
+
+env_dir File.join(prometheus_static_etc_dir, 'env') do
+  variables node['gitlab']['prometheus']['env']
+  notifies :restart, "service[prometheus]"
 end
 
 link "Link prometheus executable to correct binary" do
