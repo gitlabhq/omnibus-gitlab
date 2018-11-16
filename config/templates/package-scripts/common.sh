996371c984e52f841d3920d96e73b11246ae307a
DJ Mountney <dj@gitlab.com>
2018-11-16 07:04:19 -0800

    Flush out symlink explanation message

--- config/templates/package-scripts/common.sh
+++ config/templates/package-scripts/common.sh
@@ -1,6 +1,9 @@
-# Required for non-erroring upgrades from the old full path install log_directory
+# Required for non-erroring upgrades from versions using the full postgres version
+# in the install path. Without this we end up removing the running db's install from
+# under it, and it doesn't recover until restart, which we do not do automatically for the
+# database, causing gitlab reconfigure to fail during migrations.
 # 9.6.8 is the last postgres version where we used this old path
-# @TODO: Remove in GitLab 12
+# @TODO: Remove in GitLab 12 : https://gitlab.com/gitlab-org/omnibus-gitlab/issues/3919
 symlink_old_postgres_directory()
 {
   postgres_dir="${DEST_DIR}/embedded/postgresql/9.6"
