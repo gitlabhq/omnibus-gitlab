9ed902fc053264593f99058dfa1ce1fa2cbdbfc1
Balasankar "Balu" C <balasankar@gitlab.com>
2018-11-21 15:18:14 +0530

    Move missing redis defaults to redis attributes file

--- files/gitlab-cookbooks/gitlab-ee/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab-ee/attributes/default.rb
@@ -29,12 +29,6 @@ default['gitlab']['sentinel']['failover_timeout'] = 60000
 default['gitlab']['sentinel']['myid'] = nil
 
 ####
-# Redis Settings for EE
-####
-default['gitlab']['redis']['announce_ip'] = nil
-default['gitlab']['redis']['announce_port'] = nil
-
-####
 # Sidekiq Cluster
 ####
 default['gitlab']['sidekiq-cluster']['enable'] = false
