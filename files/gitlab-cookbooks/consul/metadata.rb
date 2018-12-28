007b3be6c8f5ec9737a30b3b30502f514a161a3d
Balasankar "Balu" C <balasankar@gitlab.com>
2018-12-28 20:44:26 +0530

    Add metadata for consul cookbook

---
+++ files/gitlab-cookbooks/consul/metadata.rb
@@ -0,0 +1,14 @@
+name 'consul'
+maintainer 'GitLab.com'
+maintainer_email 'support@gitlab.com'
+license 'Apache-2.0'
+description 'Installs/Configures consul for GitLab'
+long_description 'Installs/Configures consul for GitLab'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'package'
+depends 'gitlab'
+
+issues_url 'https://gitlab.com/gitlab-org/omnibus-gitlab/issues'
+source_url 'https://gitlab.com/gitlab-org/omnibus-gitlab'
