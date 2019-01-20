271a14b3123fd16d68f9f6d6647ed11e19331286
Balasankar "Balu" C <balasankar@gitlab.com>
2019-01-21 00:02:12 +0530

    Add tests for puma_config custom resource

---
+++ spec/fixtures/cookbooks/test_gitlab/metadata.rb
@@ -0,0 +1,2 @@
+name 'test_gitlab'
+depends 'gitlab'
---
+++ spec/fixtures/cookbooks/test_gitlab/recipes/puma_config_create.rb
@@ -0,0 +1,14 @@
+puma_config '/var/opt/gitlab/gitlab-rails/etc/puma.rb' do
+  environment 'production'
+  pid '/opt/gitlab/var/puma/puma.pid'
+  state_path '/opt/gitlab/var/puma/puma.state'
+  listen_socket '/var/opt/gitlab/gitlab-rails/sockets/gitlab.socket'
+  listen_tcp '127.0.0.1:8080'
+  working_directory '/var/opt/gitlab/gitlab-rails/working'
+  worker_processes 2
+  per_worker_max_memory_mb 650
+  install_dir '/opt/gitlab'
+  cookbook 'gitlab'
+
+  action :create
+end
