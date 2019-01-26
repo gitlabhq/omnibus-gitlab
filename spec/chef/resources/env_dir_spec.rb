72cfffafcfc81e99c01fb0c6d4e7c4e48d32a42a
Balasankar "Balu" C <balasankar@gitlab.com>
2019-01-26 22:25:44 +0530

    Modify tests of env_dir custom resource

--- spec/chef/resources/env_dir_spec.rb
+++ spec/chef/resources/env_dir_spec.rb
@@ -1,33 +1,26 @@
 require 'chef_helper'
 
 describe 'env_dir' do
-  context 'env enabled' do
-    let(:service) { 'gitaly' }
-    let(:service_env_dir) { Dir.mktmpdir }
-    let(:runner) do
-      ChefSpec::SoloRunner.new(step_into: %w(env_dir)) do |node|
-        node.normal['gitaly']['env_directory'] = service_env_dir
-        node.normal['gitaly']['env'] = { 'http_proxy' => 'test' }
-      end
-    end
-    let(:chef_run) { runner.converge("gitlab::default") }
+  let(:runner) { ChefSpec::SoloRunner.new(step_into: %w(env_dir)) }
+  let(:chef_run) { runner.converge("test_package_env_dir::create") }
 
-    before do
-      FileUtils.mkdir_p(service_env_dir)
-    end
+  after do
+    FileUtils.rm_rf('/tmp/env')
+  end
 
-    after do
-      FileUtils.rm_rf(service_env_dir)
-    end
+  it 'creates env directory' do
+    expect(chef_run).to create_directory('/tmp/env')
+  end
 
-    it 'creates env variable' do
-      expect(chef_run).to create_file("#{service_env_dir}/http_proxy").with_content('test')
-    end
+  it 'creates env variable' do
+    expect(chef_run).to create_file('/tmp/env/FOO').with_content('Lorem')
+    expect(chef_run).to create_file('/tmp/env/BAR').with_content('Ipsum')
+  end
 
-    it 'deletes extraneous files' do
-      FileUtils.touch(File.join(service_env_dir, 'erase-me'))
+  it 'deletes extraneous files' do
+    FileUtils.mkdir_p('/tmp/env')
+    FileUtils.touch('/tmp/env/erase-me')
 
-      expect(chef_run).to delete_file("#{service_env_dir}/erase-me")
-    end
+    expect(chef_run).to delete_file('/tmp/env/erase-me')
   end
 end
---
+++ spec/fixtures/cookbooks/test_package_env_dir/metadata.rb
@@ -0,0 +1,2 @@
+name 'test_package_env_dir'
+depends 'package'
