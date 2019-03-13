2e0e74625b0cba81bd8f696c4b22868ed2ab162c
Robert Marshall <rmarshall@gitlab.com>
2019-03-12 23:38:03 -0400

    Update to latest krb5 version 1.17
    - Use the upstream git repository mirror for building kerberos
    - Use a cleaner method to patch out keyutil ccache support that
      can be submitted to upstream kerberos
    
    Resolves: https://gitlab.com/gitlab-org/distribution/team-tasks/issues/295
    
    Signed-off-by: Robert Marshall <rmarshall@gitlab.com>

--- config/software/krb5.rb
+++ config/software/krb5.rb
@@ -17,30 +17,26 @@
 
 name 'krb5'
 
-default_version '1.14.2'
+default_version 'krb5-1.17'
 
 license 'MIT'
 license_file 'NOTICE'
 
 skip_transitive_dependency_licensing true
 
-source url: "http://web.mit.edu/kerberos/dist/krb5/#{version.rpartition('.').first}/krb5-#{version}.tar.gz",
-       sha256: '6bcad7e6778d1965e4ce4af21d2efdc15b274c5ce5c69031c58e4c954cda8b27'
-
-relative_path "krb5-#{version}"
+source git: "https://github.com/krb5/krb5.git"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
-  cwd = "#{Omnibus::Config.source_dir}/krb5/krb5-#{version}/src"
+  cwd = "#{Omnibus::Config.source_dir}/krb5/src"
 
-  # 'configure' will detect libkeyutils and set up the krb5 build
-  # to link against it. This gives us trouble during the Omnibus 'health
-  # check'. The patch below tries to corrupt 'configure' in such a way that
-  # 'libkeyutils' will not get added.
-  patch source: 'disable-keyutils.patch', target: 'src/configure'
+  # add a configure option allowing keyring ccache support to be
+  # disabled even if libkeyutils is detected
+  patch source: 'Add-option-to-disable-keyring-ccache-support.patch'
 
+  command "autoreconf", env: env, cwd: cwd
   command './configure' \
-           " --prefix=#{install_dir}/embedded --without-system-verto", env: env, cwd: cwd
+           " --prefix=#{install_dir}/embedded --without-system-verto --disable-keyring-ccache", env: env, cwd: cwd
 
   make " -j #{workers}", env: env, cwd: cwd
   make 'install', env: env, cwd: cwd
