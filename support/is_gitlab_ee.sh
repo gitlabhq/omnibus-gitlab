7ae47fe29159848d7143393fa36fa9037b8b54a0
John Jarvis <jarv@gitlab.com>
2019-04-17 15:48:16 +0200

    Add auto-deploy to `is_gitlab_ee`

--- support/is_gitlab_ee.sh
+++ support/is_gitlab_ee.sh
@@ -8,8 +8,11 @@ function ee_branch_var(){
   [[ -n "${GITLAB_VERSION}" ]] && [[ "${GITLAB_VERSION}" == *-ee ]]
 }
 
-function  ee_branch_name(){
+function ee_branch_name(){
   grep -q -E "\-ee" VERSION
 }
 
-ee_env_var || ee_branch_var || ee_branch_name
+function is_auto_deploy(){
+  echo "$CI_COMMIT_TAG" | grep -q -E '^\d+\.\d+\.[^ ]+\+[^ ]+$'
+}
+ee_env_var || ee_branch_var || ee_branch_name || is_auto_deploy
