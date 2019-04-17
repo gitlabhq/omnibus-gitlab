33a974f3f68db33807e5c40331d25684aa206366
John Jarvis <jarv@gitlab.com>
2019-04-17 16:15:24 +0200

    Add some debug

--- support/is_gitlab_ee.sh
+++ support/is_gitlab_ee.sh
@@ -12,7 +12,14 @@ function ee_branch_name(){
   grep -q -E "\-ee" VERSION
 }
 
-function is_auto_deploy(){
+function is_auto_deploy_tag(){
   echo "$CI_COMMIT_TAG" | grep -q -E '^\d+\.\d+\.[^ ]+\+[^ ]+$'
 }
+
+function is_auto_deploy_branch(){
+  echo "$CI_COMMIT_TAG" | grep -q -E '^\d+\.\d+\.[^ ]+\+[^ ]+$'
+}
+echo "commit tag: $CI_COMMIT_TAG"
+echo "commit ref: $CI_COMMIT_REF_NAME"
+
 ee_env_var || ee_branch_var || ee_branch_name || is_auto_deploy
