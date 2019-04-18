86db7b2724632b8a4b6933f7fb0b8a77da11331e
John Jarvis <jarv@gitlab.com>
2019-04-18 17:19:29 +0200

    Fixing tests

--- lib/gitlab/takeoff_helper.rb
+++ lib/gitlab/takeoff_helper.rb
@@ -12,11 +12,9 @@ class TakeoffHelper
 
   def trigger_deploy
     form_data = {
-      "token" => @trigger_token,
-      "ref" => @trigger_ref,
-      "variables[DEPLOY_ENVIRONMENT]" => @deploy_env,
-      "variables[DEPLOY_VERSION]" => 'some-version-that-does-not-exist'
-    }
+      'token' => @trigger_token,
+      'ref' => @trigger_ref,
+    }.merge(takeoff_env_vars.map{ |k,v| ["variables[#{k}]",v] }.to_h)
     resp = HTTP.post(pipeline_trigger_url, :form => form_data)
     raise "Unable to trigger #{pipeline_url}, status: #{resp.status}" unless resp.status == 201
     JSON.parse(resp.body.to_s)['web_url']
@@ -41,11 +39,10 @@ class TakeoffHelper
 
   def takeoff_env_vars
     {
-      'DEPLOY_ENVIRONMENT': @deploy_env,
-      'DEPLOY_VERSION': Gitlab::Util.get_env('TAKEOFF_VERSION') || release,
-      'DEPLOY_REPO': Gitlab::Util.get_env('TAKEOFF_DEPLOY_REPO') || 'gitlab/pre-release',
-      'DEPLOY_USER': Gitlab::Util.get_env('TAKEOFF_DEPLOY_USER') || 'takeoff',
-      'CHECKMODE': '--check'
+      'DEPLOY_ENVIRONMENT' => @deploy_env,
+      'DEPLOY_VERSION' => Gitlab::Util.get_env('TAKEOFF_VERSION') || release,
+      'DEPLOY_REPO' => Gitlab::Util.get_env('TAKEOFF_DEPLOY_REPO') || 'gitlab/pre-release',
+      'DEPLOY_USER' => Gitlab::Util.get_env('TAKEOFF_DEPLOY_USER') || 'takeoff'
     }
   end
 end
--- spec/gitlab/takeoff_helper_spec.rb
+++ spec/gitlab/takeoff_helper_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
+require 'gitlab/build'
 require 'gitlab/takeoff_helper'
 
 describe TakeoffHelper  do
-  subject(:service) { described_class.new('gstg', 'token', 'master') }
+  subject(:service) { described_class.new('some-token', 'some-env', 'some-branch') }
   describe '#trigger_deploy' do
     it 'triggers an auto deploy' do
       response = instance_double('response', body: JSON.dump(web_url: 'http://example.com'), status: 201)
+      allow(Build::Info).to receive(:docker_tag).and_return('some-version')
       expect(HTTP)
         .to receive(:post)
         .with(
           "https://ops.gitlab.net/api/v4/projects/135/trigger/pipeline",
           form: {
-            "token" => "gstg",
-            "ref" => "master",
-            "variables[DEPLOY_ENVIRONMENT]"=>"pre",
-            "variables[DEPLOY_VERSION]"=>"some-version-that-does-not-exist",
-            "variables[CHECKMODE]"=>"--check"
+            "token" => "some-token",
+            "ref" => "some-branch",
+            "variables[DEPLOY_ENVIRONMENT]"=>"some-env",
+            "variables[DEPLOY_VERSION]"=>"some-version",
+            "variables[DEPLOY_REPO]"=>"gitlab/pre-release",
+            "variables[DEPLOY_USER]"=>"takeoff"
           }
         ).and_return(response)
       expect(service.trigger_deploy).to eq('http://example.com')
