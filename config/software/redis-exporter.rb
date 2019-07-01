c1a5db52022aeb55e8f7963e347bf35f5d2d6387
Ben Kochie <bjk@gitlab.com>
2019-07-01 18:02:19 +0200

    Update Redis and redis-exporter versions
    * Update Redis to 5.0.7.
    * Update redis_exporter to 1.3.4.

--- config/software/redis-exporter.rb
+++ config/software/redis-exporter.rb
@@ -20,7 +20,7 @@ require "#{Omnibus::Config.project_root}/lib/gitlab/version"
 require 'time'
 
 name 'redis-exporter'
-version = Gitlab::Version.new('redis-exporter', '1.1.0')
+version = Gitlab::Version.new('redis-exporter', '1.3.4')
 default_version version.print
 
 license 'MIT'
