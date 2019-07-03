cfeb499c15617b4735378d6ae4be1d8b2780f682
Balasankar "Balu" C <balasankar@gitlab.com>
2019-07-03 12:28:06 +0530

    Add specs for postgresql username detection
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

---
+++ spec/gitlab-ctl-commands/lib/postgresql_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+$LOAD_PATH << File.join(__dir__, '../../../files/gitlab-ctl-commands/lib')
+
+require 'postgresql'
+
+describe GitlabCtl::PostgreSQL do
+  describe "#postgresql_usernamename" do
+    context 'when using legacy configuration' do
+      before do
+        allow(GitlabCtl::Util).to receive(:get_node_attributes).and_return(
+          {
+            'gitlab' => {
+              'postgresql' => {
+                'username' => 'foo'
+              }
+            }
+          }
+        )
+      end
+      it 'detects username correctly' do
+        expect(described_class.postgresql_username).to eq('foo')
+      end
+    end
+
+    context 'when using new configuration' do
+      before do
+        allow(GitlabCtl::Util).to receive(:get_node_attributes).and_return(
+          {
+            'postgresql' => {
+              'username' => 'bar'
+            }
+          }
+        )
+      end
+      it 'detects username correctly' do
+        expect(described_class.postgresql_username).to eq('bar')
+      end
+    end
+  end
+end
