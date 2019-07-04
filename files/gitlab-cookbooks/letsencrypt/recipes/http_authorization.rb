6ac89269280ab5eb60734d37f56bd49c5d29155e
Stan Hu <stanhu@gmail.com>
2019-07-04 00:40:59 -0700

    Fix GitLab upgrades crashing when LetsEncrypt cert needs renewal
    During an upgrade, `upgrade.rb` runs and shuts down all services not
    essential to database migrations, including NGINX. `reconfigure(false)`
    is then run. If an existing installation already has its LetsEncrypt
    certificates on disk, NGINX is never started, and the LetsEncrypt HTTP
    authorization check fails. This causes the GitLab upgrade to fail.
    
    To fix this issue, we only attempt the LetsEncrypt HTTP authorization if
    NGINX is running.
    
    Closes https://gitlab.com/gitlab-org/omnibus-gitlab/issues/4471

--- files/gitlab-cookbooks/letsencrypt/recipes/http_authorization.rb
+++ files/gitlab-cookbooks/letsencrypt/recipes/http_authorization.rb
@@ -1,8 +1,11 @@
 site = URI(node['gitlab']['external-url']).host
 
+omnibus_helper = OmnibusHelper.new(node)
+
 letsencrypt_certificate site do
   fullchain node['gitlab']['nginx']['ssl_certificate']
   key node['gitlab']['nginx']['ssl_certificate_key']
   notifies :run, "execute[reload nginx]", :immediate
   notifies :run, 'ruby_block[display_le_message]'
+  only_if { omnibus_helper.service_up?('nginx') }
 end
