b9863882493e16f84c3123487a53d09c9a0442d4
DJ Mountney <dj@gitlab.com>
2019-08-21 00:37:09 +0000

    Merge branch 'jts/fix-lengthy-filenames' into 'master'
    Clean up directories that should not exists in the QA image
    
    See merge request gitlab-org/omnibus-gitlab!3531

--- lib/gitlab/build/qa.rb
+++ lib/gitlab/build/qa.rb
@@ -14,7 +14,9 @@ module Build
     def self.get_gitlab_repo
       clone_gitlab_rails
       checkout_gitlab_rails
-      File.absolute_path("#{repo_path}/qa")
+      cleanup_directories
+
+      repo_path
     end
 
     def self.clone_gitlab_rails
@@ -47,6 +49,10 @@ module Build
       puts "Building from #{Build::Info.package} commit #{version}"
 
       system(*%W[git --git-dir=#{repo_path}/.git --work-tree=#{repo_path} checkout --quiet #{version}])
+    end
+
+    def self.cleanup_directories
+      system(*%W[rm -rf #{repo_path}/ee/changelogs/unreleased])
     end
   end
 end
