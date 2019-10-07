212ac2b12d635c1335a0f630e351ba6f4618681b
Balasankar "Balu" C <balasankar@gitlab.com>
2019-10-07 13:12:52 +0530

    Fix omnibus-gitlab-mirror path in HA validate trigger
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/gitlab/build/ha_validate/trigger.rb
+++ lib/gitlab/build/ha_validate/trigger.rb
@@ -1,6 +1,7 @@
 require_relative '../trigger'
 require_relative '../info'
 require_relative "../../util.rb"
+require 'cgi'
 
 module Build
   class HA
@@ -8,21 +9,26 @@ module Build
       extend Trigger
 
       PROJECT_PATH = 'gitlab-org/distribution/gitlab-provisioner'.freeze
+      OMNIBUS_PROJECT_MIRROR_PATH = 'gitlab-org/build/omnibus-gitlab-mirror'.freeze
 
       def self.get_project_path
         PROJECT_PATH
       end
 
+      def self.omnibus_gitlab_path
+        CGI.escape(OMNIBUS_PROJECT_MIRROR_PATH)
+      end
+
       def self.ee_package_job_id
         Build::Info.fetch_pipeline_jobs(
-          'gitlab-org%2Fomnibus-gitlab',
+          omnibus_gitlab_path,
           Gitlab::Util.get_env('CI_PIPELINE_ID'),
           Gitlab::Util.get_env('HA_VALIDATE_TOKEN')
         ).select { |job| job['name'] == 'Trigger:package' }.first['id']
       end
 
       def self.get_params(image: nil)
-        qa_image = image || "registry.gitlab.com/gitlab-org/omnibus-gitlab/gitlab-ee-qa:omnibus-#{Gitlab::Util.get_env('CI_COMMIT_SHA')}"
+        qa_image = image || "registry.gitlab.com/#{OMNIBUS_PROJECT_MIRROR_PATH}/gitlab-ee-qa:omnibus-#{Gitlab::Util.get_env('CI_COMMIT_SHA')}"
         {
           'ref' => 'master',
           'token' => Gitlab::Util.get_env('HA_VALIDATE_TOKEN'),
