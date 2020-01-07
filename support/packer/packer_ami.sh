0b260007f43b11d80c7d544115aa4d9ff862cd99
Balasankar "Balu" C <balasankarc@autistici.org>
2020-01-07 16:11:03 +0530

    Build AMI for GitLab Premium
    Signed-off-by: Balasankar "Balu" C <balasankarc@autistici.org>

--- support/packer/packer_ami.sh
+++ support/packer/packer_ami.sh
@@ -3,9 +3,10 @@
 VERSION=$1
 TYPE=$2
 DOWNLOAD_URL=$3
+EE_LICENSE_FILE=$4
 
 PACKER_PATH=$(pwd)/support/packer
 
 cd $PACKER_PATH
 
-packer build -var "aws_access_key=$AWS_AMI_ACCESS_KEY_ID" -var "aws_secret_key=$AWS_AMI_SECRET_ACCESS_KEY" -var "version=$VERSION" -var "download_url=$DOWNLOAD_URL" -var "license_file=$AMI_LICENSE_FILE" $PACKER_PATH/$TYPE.json
+packer build -var "aws_access_key=$AWS_AMI_ACCESS_KEY_ID" -var "aws_secret_key=$AWS_AMI_SECRET_ACCESS_KEY" -var "version=$VERSION" -var "download_url=$DOWNLOAD_URL" -var "license_file=$EE_LICENSE_FILE" $PACKER_PATH/$TYPE.json
