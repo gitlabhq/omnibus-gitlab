ab3f671461329968412f173dc8d18464e9024767
Harrison Healey <harrisonmhealey@gmail.com>
2020-01-09 09:46:33 -0500

    Update Mattermost to 5.17.3 (GitLab 12.6)

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -17,10 +17,10 @@
 #
 
 name 'mattermost'
-default_version '5.17.2'
+default_version '5.17.3'
 
 source url: "https://releases.mattermost.com/#{version}/mattermost-team-#{version}-linux-amd64.tar.gz",
-       md5: '7e23ee7853f563f8948a28bf696604fb'
+       md5: '1d7ff8d9cfecef50a75fec699047dfd0'
 
 relative_path 'mattermost'
 
