9835eb18e4b5bb7e142992c879a9690eb02a7437
DJ Mountney <dj@gitlab.com>
2020-04-17 05:26:40 +0000

    Fix misformatted error
    Ensure the proper error is thrown

--- files/gitlab-ctl-commands/lib/gitlab_ctl/pg_upgrade.rb
+++ files/gitlab-ctl-commands/lib/gitlab_ctl/pg_upgrade.rb
@@ -127,7 +127,7 @@ module GitlabCtl
           false
         end
       end
-        raise GitlabCtl::Errors::ExecutionError, 'Error upgrading the database'
+        raise GitlabCtl::Errors::ExecutionError, 'run_pg_upgrade', '', 'Error upgrading the database'
       end
     end
 
