eb0e5a941fde04d63e48aea23412ca8b097cba92
Alex Ives <aives@gitlab.com>
2020-06-15 10:49:12 -0500

    Add pause/resume replication commands to gitlab-ctl
    - Update POC code to also call rake task that calls primary api to pause
    - Add changelog
    
    Relates to https://gitlab.com/gitlab-org/omnibus-gitlab/issues/35913

--- files/gitlab-ctl-commands-ee/lib/geo/replication_process.rb
+++ files/gitlab-ctl-commands-ee/lib/geo/replication_process.rb
@@ -1,12 +1,4 @@
-require 'io/console'
 require 'rainbow/ext/string'
-
-# For testing purposes, if the first path cannot be found load the second
-begin
-  require_relative '../../../omnibus-ctl/lib/postgresql'
-rescue LoadError
-  require_relative '../../../gitlab-ctl-commands/lib/postgresql'
-end
 
 module Geo
   class ReplicationProcess
@@ -21,25 +13,49 @@ module Geo
 
     def pause
       puts '* Pausing replication'.color(:green)
-      run_query('SELECT pg_wal_replay_pause();')
+
+      task = run_task('geo:replication:pause')
+      # This isn't an error because theoretically if the primary is down you
+      # may want to use this to pause the WAL anyway.
+      puts task.stdout.strip.color(:red) if task.error?
+
+      query = run_query('SELECT pg_wal_replay_pause();')
+      raise PsqlError, "Unable to pause postgres replication #{query.stdout.strip}" if query.error?
+
+      puts '* Replication paused'.color(:green)
     end
 
     def resume
       puts '* Resume replication'.color(:green)
-      run_query('pg_wal_replay_resume();')
+
+      query = run_query('pg_wal_replay_resume();')
+      raise PsqlError, "Unable to resume postgres replication #{query.stdout.strip}" if query.error?
+
+      task = run_task('geo:replication:resume')
+      raise RakeError, "Unable to resume replication from primary #{task.stdout.strip}" if task.error?
+
+      puts '* Replication resumed'.color(:green)
     end
 
     private
 
     def run_query(query)
-      status = GitlabCtl::Util.run_command(
+      GitlabCtl::Util.run_command(
         "#{base_path}/bin/gitlab-psql -d #{db_name} -c '#{query}' -q -t"
       )
-      status.error? ? false : status.stdout.strip
+    end
+
+    def run_task(task)
+      GitlabCtl::Util.run_command(
+        "#{base_path}/bin/gitlab-rake #{task}"
+      )
     end
 
     def db_name
       @options[:db_name]
     end
   end
+
+  PsqlError = Class.new(StandardError)
+  RakeError = Class.new(StandardError)
 end
