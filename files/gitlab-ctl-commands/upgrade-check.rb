5575ed8364bfd25ea4f74db3d56354eddccd1e2b
Ian Baum <ibaum@gitlab.com>
2020-07-10 08:19:24 -0500

    Changes per MR
    * Edit changelog language to be more accurate
    * Pass arguments to gitlab-ctl upgrade-check as expected
    * Handle new installs better

--- files/gitlab-ctl-commands/upgrade-check.rb
+++ files/gitlab-ctl-commands/upgrade-check.rb
@@ -1,9 +1,11 @@
 require "#{base_path}/embedded/service/omnibus-ctl/lib/gitlab_ctl/upgrade_check"
 
 add_command('upgrade-check', 'Check if the upgrade is acceptable', 2) do
-  unless GitlabCtl::UpgradeCheck.valid?(ARGV[2], ARGV[3])
-    warn "It seems you are upgrading from major version ${OLD_MAJOR_VERSION} to major version ${NEW_MAJOR_VERSION}."
-    warn "It is required to upgrade to the latest ${MIN_VERSION}.x version first before proceeding."
+  old_version = ARGV[3]
+  new_version = ARGV[4]
+  unless GitlabCtl::UpgradeCheck.valid?(old_version, new_version)
+    warn "It seems you are upgrading from major version #{old_version} to major version #{new_version}."
+    warn "It is required to upgrade to the latest #{GitlabCtl::UpgradeCheck::MIN_VERSION}.x version first before proceeding."
     warn "Please follow the upgrade documentation at https://docs.gitlab.com/ee/policy/maintenance.html#upgrading-major-versions"
     Kernel.exit 1
   end
