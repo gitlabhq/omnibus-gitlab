8aca460d7c02bb5507e38675024f40323ca1304a
Ian Baum <ibaum@gitlab.com>
2020-07-13 14:19:35 -0500

    Update upgrade_check output to use major versions

--- files/gitlab-ctl-commands/upgrade-check.rb
+++ files/gitlab-ctl-commands/upgrade-check.rb
@@ -4,7 +4,7 @@ add_command('upgrade-check', 'Check if the upgrade is acceptable', 2) do
   old_version = ARGV[3]
   new_version = ARGV[4]
   unless GitlabCtl::UpgradeCheck.valid?(old_version, new_version)
-    warn "It seems you are upgrading from major version #{old_version} to major version #{new_version}."
+    warn "It seems you are upgrading from major version #{old_version.split('.').first} to major version #{new_version.split('.').first}."
     warn "It is required to upgrade to the latest #{GitlabCtl::UpgradeCheck::MIN_VERSION}.x version first before proceeding."
     warn "Please follow the upgrade documentation at https://docs.gitlab.com/ee/policy/maintenance.html#upgrading-major-versions"
     Kernel.exit 1
