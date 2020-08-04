2ce4938a3065b9c8bcbb617c29cd0f7fab9ee801
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2020-08-04 18:01:14 -0300

    Remove FdwHelper#fdw_can_refresh? method
    Since Gitlab 13.2 Geo does not rely on the
    Foreign Data Wrapper to replicate data.

--- files/gitlab-cookbooks/gitlab-ee/libraries/fdw_helper.rb
+++ files/gitlab-cookbooks/gitlab-ee/libraries/fdw_helper.rb
@@ -12,35 +12,6 @@ class FdwHelper
     node['gitlab']['geo-secondary']['db_fdw']
   end
 
-  def fdw_can_refresh?
-    fdw_enabled? &&
-      gitlab_geo_helper.geo_database_configured? &&
-      !pg_helper.is_managed_and_offline? &&
-      !pg_helper.database_empty?(fdw_dbname) &&
-      !geo_pg_helper.is_offline_or_readonly? &&
-      geo_pg_helper.schema_exists?(FOREIGN_SCHEMA, tracking_dbname) &&
-      !foreign_schema_tables_match?
-  end
-
-  # Check if foreign schema has exact the same tables and fields defined on secondary database
-  #
-  # @return [Boolean] whether schemas match
-  def foreign_schema_tables_match?
-    retrieve_gitlab_schema_tables == retrieve_foreign_schema_tables
-  end
-
-  def retrieve_foreign_schema_tables
-    geo_pg_helper.retrieve_schema_tables(FOREIGN_SCHEMA, tracking_dbname)
-  end
-
-  def retrieve_gitlab_schema_tables
-    pg_helper.retrieve_schema_tables(DEFAULT_SCHEMA, fdw_dbname)
-  end
-
-  def tracking_dbname
-    node['gitlab']['geo-secondary']['db_database']
-  end
-
   def fdw_dbname
     node['gitlab']['gitlab-rails']['db_database']
   end
@@ -73,19 +44,5 @@ class FdwHelper
                    })
     end
     entries
-  end
-
-  private
-
-  def gitlab_geo_helper
-    @gitlab_geo_helper ||= GitlabGeoHelper.new(node)
-  end
-
-  def geo_pg_helper
-    @geo_pg_helper ||= GeoPgHelper.new(node)
-  end
-
-  def pg_helper
-    @pg_helper ||= PgHelper.new(node)
   end
 end unless defined?(FdwHelper) # Prevent reloading in chefspec: https://github.com/sethvargo/chefspec/issues/562#issuecomment-74120922
