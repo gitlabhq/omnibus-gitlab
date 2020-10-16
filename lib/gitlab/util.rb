c97f7d4ad6319486ccc89229178e8b26028fd319
Robert Speicher <rspeicher@gmail.com>
2020-10-16 14:03:24 -0600

    Add section, section_start and section_end helpers

--- lib/gitlab/util.rb
+++ lib/gitlab/util.rb
@@ -12,6 +12,27 @@ module Gitlab
       def set_env_if_missing(key, value)
         ENV[key] ||= value&.strip
       end
+
+      def section(name, description = name, &block)
+        section_start(name, description)
+
+        yield
+
+        section_end(name)
+      end
+
+      def section_start(name, description = name)
+        return unless ENV['CI']
+
+        @section_name = name
+        $stdout.puts "section_start:#{Time.now.to_i}:#{name}\r\e[0K#{description}"
+      end
+
+      def section_end(name = @section_name)
+        return unless ENV['CI']
+
+        $stdout.puts "section_end:#{Time.now.to_i}:#{name}\r\e[0K"
+      end
     end
   end
 end
