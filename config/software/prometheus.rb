f8961643cd7d3b760898be1c0532ec719044786f
Ben Kochie <bjk@gitlab.com>
2020-10-21 16:26:49 +0200

    Update Prometheus components
    * Update to latest Prometheus 2.22.0.
    * Update to latest Redis exporter.
    
    Mostly bug fixes and performance improvements. Prometheus 2.22.0
    improves storage compression in some cases.
    
    Signed-off-by: Ben Kochie <bjk@gitlab.com>

--- config/software/prometheus.rb
+++ config/software/prometheus.rb
@@ -20,7 +20,7 @@ require "#{Omnibus::Config.project_root}/lib/gitlab/version"
 require "#{Omnibus::Config.project_root}/lib/gitlab/prometheus_helper"
 
 name 'prometheus'
-version = Gitlab::Version.new('prometheus', '2.20.1')
+version = Gitlab::Version.new('prometheus', '2.22.0')
 default_version version.print
 
 license 'APACHE-2.0'
