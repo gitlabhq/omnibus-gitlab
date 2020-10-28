ba22dc6ee70ba8dca10bd267d2790459e523a88e
Valery Sizov <valery@gitlab.com>
2020-10-28 19:23:19 +0000

    Geo: Implement PITR recovery before promotion of secondary node
    If replication was paused on secondary, we need to use the last point
    in time for the database when we promote the node.

--- files/gitlab-ctl-commands-ee/lib/geo/replication_toggle_command.rb
+++ files/gitlab-ctl-commands-ee/lib/geo/replication_toggle_command.rb
@@ -1,4 +1,5 @@
 require_relative "./replication_process"
+require_relative "./promote_db"
 require 'optparse'
 require 'English'
 
@@ -19,6 +20,8 @@ module Geo
 
     def execute!
       @replication_process.send(@action.to_sym)
+
+      process_pitr_file
     rescue Geo::PsqlError => e
       puts "Postgres encountered an error: #{e.message}"
       exit 1
@@ -32,6 +35,30 @@ module Geo
     end
 
     private
+
+    attr_reader :action, :ctl
+
+    def process_pitr_file
+      geo_pitr_file_path = "#{ctl.data_path}/postgresql/data/#{Geo::PromoteDb::PITR_FILE_NAME}"
+
+      if action == 'pause'
+        puts "* Create Geo point-in-time recovery file".color(:green)
+
+        File.write(geo_pitr_file_path, current_lsn)
+      elsif action == 'resume'
+        puts "* Remove Geo point-in-time recovery file".color(:green)
+
+        File.delete(geo_pitr_file_path) if File.exist?(geo_pitr_file_path)
+      end
+    end
+
+    def current_lsn
+      run_query('SELECT pg_last_wal_replay_lsn()')
+    end
+
+    def run_query(query)
+      GitlabCtl::Util.get_command_output("gitlab-psql -d postgres -c '#{query}' -q -t").strip
+    end
 
     def parse_options!
       opts_parser = OptionParser.new do |opts|
