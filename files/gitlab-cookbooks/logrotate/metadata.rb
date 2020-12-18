d7de5f9ee229e7f5187602aa6fed2ffef7349c9a
Gabriel Mazetto <gabriel@gitlab.com>
2020-12-18 11:37:59 -0300

    Add logrotate cookbook metadata

---
+++ files/gitlab-cookbooks/logrotate/metadata.rb
@@ -0,0 +1,14 @@
+name 'logrotate'
+maintainer 'GitLab.com'
+maintainer_email 'support@gitlab.com'
+license 'Apache 2.0'
+description 'Installs/Configures logrotate for GitLab'
+long_description 'Installs/Configures logrotate for GitLab'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+issues_url 'https://gitlab.com/gitlab-org/omnibus-gitlab/issues'
+source_url 'https://gitlab.com/gitlab-org/omnibus-gitlab'
+
+depends 'package'
+depends 'gitlab'
