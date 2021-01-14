53c666d8bce95ed7e9dc82bf81e7b8fbdf312313
Hossein Pursultani <hpursultani@gitlab.com>
2021-01-14 11:23:31 +1100

    Safely lookup PostgreSQL parameters in Patroni settings

--- files/gitlab-cookbooks/gitlab/libraries/patroni.rb
+++ files/gitlab-cookbooks/gitlab/libraries/patroni.rb
@@ -32,7 +32,7 @@ module Patroni
     ).freeze
 
     def postgresql_setting(key)
-      Gitlab['postgresql'][key] || Gitlab['node']['patroni']['postgresql'][key] || Gitlab['node']['postgresql'][key]
+      Gitlab['postgresql'][key] || Gitlab['node']['patroni']['postgresql']&.[](key) || Gitlab['node']['postgresql'][key]
     end
 
     # These attributes are the postgres settings that patroni manages through its DCS,
