7926d212f199bff8ed3702be06b8adcabb3463be
Stan Hu <stanhu@gmail.com>
2021-03-24 07:27:27 -0700

    Add mail_room as a separate Gem dependency
    mail_room has its own dependencies and isn't actually needed in GitLab
    Rails, so we should split this out into its own software install.
    
    This would have avoided
    https://gitlab.com/gitlab-com/gl-infra/production/-/issues/4053.
    
    Changelog: changed

---
+++ config/software/mail_room.rb
@@ -0,0 +1,30 @@
+#
+# Copyright:: Copyright (c) 2021 GitLab Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name 'mail_room'
+default_version '0.0.10'
+
+license 'MIT'
+license_file 'LICENSE.txt'
+
+skip_transitive_dependency_licensing true
+
+dependency 'ruby'
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+  gem "install gitlab-mail_room --no-document --version #{version}", env: env
+end
