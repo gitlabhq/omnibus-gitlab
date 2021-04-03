3b6a41eb2c90f0f5844d93c696048d8eaa3b2b14
Stan Hu <stanhu@gmail.com>
2021-04-02 17:00:50 -0700

    Add Microsoft Graph support for MailRoom
    This commit adds support for Microsoft Graph API with OAuth2 to
    MailRoom.
    
    The config file in
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/58250 is necessary
    for this to work.
    
    References:
    
    1. https://gitlab.com/gitlab-org/gitlab/-/issues/214900
    2. https://github.com/tpitale/mail_room/pull/125
    
    Changelog: added
    
    Signed-off-by: Stan Hu <stanhu@gmail.com>

--- files/gitlab-cookbooks/gitlab/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab/attributes/default.rb
@@ -158,6 +158,9 @@ default['gitlab']['gitlab-rails']['incoming_email_mailbox_name'] = "inbox"
 default['gitlab']['gitlab-rails']['incoming_email_idle_timeout'] = nil
 default['gitlab']['gitlab-rails']['incoming_email_log_file'] = "/var/log/gitlab/mailroom/mail_room_json.log" # file path of internal `mail_room` JSON logs
 default['gitlab']['gitlab-rails']['incoming_email_expunge_deleted'] = nil
+default['gitlab']['gitlab-rails']['incoming_email_inbox_method'] = "imap"
+default['gitlab']['gitlab-rails']['incoming_email_inbox_options'] = nil
+
 default['gitlab']['gitlab-rails']['service_desk_email_enabled'] = false
 default['gitlab']['gitlab-rails']['service_desk_email_address'] = nil
 default['gitlab']['gitlab-rails']['service_desk_email_host'] = nil
@@ -169,6 +172,8 @@ default['gitlab']['gitlab-rails']['service_desk_email_password'] = nil
 default['gitlab']['gitlab-rails']['service_desk_email_mailbox_name'] = "inbox"
 default['gitlab']['gitlab-rails']['service_desk_email_idle_timeout'] = nil
 default['gitlab']['gitlab-rails']['service_desk_email_log_file'] = "/var/log/gitlab/mailroom/mail_room_json.log" # file path of internal `mail_room` JSON logs
+default['gitlab']['gitlab-rails']['service_desk_email_inbox_method'] = "imap"
+default['gitlab']['gitlab-rails']['service_desk_email_inbox_inbox_options'] = nil
 
 # Consolidated object storage config
 default['gitlab']['gitlab-rails']['object_store']['enabled'] = false
--- spec/chef/recipes/mailroom_spec.rb
+++ spec/chef/recipes/mailroom_spec.rb
@@ -17,7 +17,7 @@
 require 'chef_helper'
 
 RSpec.describe 'gitlab::mailroom' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(step_into: %w(runit_service)).converge('gitlab::default') }
+  let(:chef_run) { ChefSpec::SoloRunner.new(step_into: %w(runit_service templatesymlink)).converge('gitlab::default') }
 
   before do
     allow(Gitlab).to receive(:[]).and_call_original
@@ -83,6 +83,46 @@ RSpec.describe 'gitlab::mailroom' do
       end
 
       it_behaves_like 'enabled runit service', 'mailroom', 'root', 'root'
+    end
+
+    shared_examples 'renders Microsoft Graph config' do
+      let(:gitlab_yml_template) { chef_run.template('/var/opt/gitlab/gitlab-rails/etc/gitlab.yml') }
+      let(:gitlab_yml_file_content) { ChefSpec::Renderer.new(chef_run, gitlab_yml_template).content }
+      let(:gitlab_yml) { YAML.safe_load(gitlab_yml_file_content, [], [], true, symbolize_names: true) }
+      let(:inbox_options) do
+        {
+          tenant_id: 'MY-TENANT-ID',
+          client_id: '12345',
+          client_secret: 'MY-CLIENT-SECRET'
+        }
+      end
+
+      before do
+        stub_gitlab_rb(
+          gitlab_rails: {
+            "#{config_section}_enabled".to_sym => true,
+            "#{config_section}_inbox_method".to_sym => 'microsoft_graph',
+            "#{config_section}_inbox_options".to_sym => inbox_options
+          }
+        )
+      end
+
+      it 'renders gitlab.yml with the right data' do
+        expect(gitlab_yml[:production][config_section][:inbox_method]).to eq('microsoft_graph')
+        expect(gitlab_yml[:production][config_section][:inbox_options]).to eq(inbox_options)
+      end
+    end
+
+    context 'incoming email with Microsoft Graph' do
+      let(:config_section) { :incoming_email }
+
+      it_behaves_like 'renders Microsoft Graph config'
+    end
+
+    context 'Service Desk email with Microsoft Graph' do
+      let(:config_section) { :service_desk_email }
+
+      it_behaves_like 'renders Microsoft Graph config'
     end
   end
 
