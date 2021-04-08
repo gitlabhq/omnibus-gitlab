94b691bdbe5feed8d698a48eb0019a438992ba7a
Robert Marshall <rmarshall@gitlab.com>
2021-04-08 15:54:48 +0000

    Do not depend on truthy strings in config

--- omnibus.rb
+++ omnibus.rb
@@ -1,9 +1,9 @@
-use_s3_caching ENV['USE_S3_CACHE'] || false
+use_s3_caching !ENV['USE_S3_CACHE'].nil? && !ENV['USE_S3_CACHE'].casecmp("false").zero?
 s3_access_key ENV['CACHE_AWS_ACCESS_KEY_ID']
 s3_secret_key ENV['CACHE_AWS_SECRET_ACCESS_KEY']
 s3_bucket ENV['CACHE_AWS_BUCKET']
 s3_region ENV['CACHE_AWS_S3_REGION']
-s3_accelerate ENV['CACHE_S3_ACCELERATE'] || false
+s3_accelerate !ENV['CACHE_S3_ACCELERATE'].nil? && !ENV['CACHE_S3_ACCELERATE'].casecmp("false").zero?
 
 build_retries 2
 fetcher_retries 5
