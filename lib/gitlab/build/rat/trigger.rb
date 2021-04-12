c30ca083f0bb6d6cdee469e3875fef00b119a165
Balasankar "Balu" C <balasankar@gitlab.com>
2021-04-12 14:57:06 +0530

    Add code to trigger a RAT pipeline
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

---
+++ lib/gitlab/build/rat/trigger.rb
@@ -0,0 +1,36 @@
+require_relative '../trigger'
+require_relative '../info'
+require_relative "../../util"
+require 'cgi'
+
+module Build
+  class RAT
+    class PipelineTrigger
+      extend Trigger
+
+      PROJECT_PATH = 'gitlab-org/distribution/reference-architecture-tester'.freeze
+
+      def self.get_project_path
+        PROJECT_PATH
+      end
+
+      def self.omnibus_gitlab_path
+        CGI.escape(Build::Info::OMNIBUS_PROJECT_MIRROR_PATH)
+      end
+
+      def self.get_params(image: nil)
+        {
+          'ref' => 'master',
+          'token' => Gitlab::Util.get_env('RAT_TRIGGER_TOKEN'),
+          'variables[REFERENCE_ARCHITECTURE]' => 'omnibus-gitlab-mrs',
+          'variables[PACKAGE_URL]' => Gitlab::Util.get_env('PACKAGE_URL') || Build::Info.triggered_build_package_url,
+          'variables[QA_IMAGE]' => Gitlab::Util.get_env('QA_IMAGE') || image || "registry.gitlab.com/#{Build::Info::OMNIBUS_PROJECT_MIRROR_PATH}/gitlab-ee-qa:#{Build::Info.docker_tag}"
+        }
+      end
+
+      def self.get_access_token
+        Gitlab::Util.get_env('GITLAB_BOT_MULTI_PROJECT_PIPELINE_POLLING_TOKEN')
+      end
+    end
+  end
+end
