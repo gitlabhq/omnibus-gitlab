58cdb7aa419efb040863c9b2bdc9f4365fbaa8c4
Jason Young <jyoung@gitlab.com>
2021-05-13 05:18:05 +0000

    Use backup_keep_time to prune configuration backups
    Changelog: changed

--- files/gitlab-ctl-commands/backup.rb
+++ files/gitlab-ctl-commands/backup.rb
@@ -18,6 +18,36 @@
 require "#{base_path}/embedded/service/omnibus-ctl/lib/gitlab_ctl/backup"
 
 add_command_under_category('backup-etc', 'backup',
-                           'Backup GitLab configuration [accepts directory path]', 2) do |cmd_name, backup_dir_path|
-  GitlabCtl::Backup.perform backup_dir_path
+                           'Backup GitLab configuration [options]', 2) do |cmd_name, *args|
+  def get_ctl_options
+    options = {}
+    leftover_args = OptionParser.new do |opts|
+      opts.banner = "Usage: gitlab-ctl backup-etc [options]"
+
+      opts.on('--[no-]delete-old-backups', 'Delete backups older than the specified "backup_keep_time" setting') do |delete_old_backups|
+        options[:delete_old_backups] = delete_old_backups
+      end
+
+      opts.on('-p', '--backup-path BACKUP_PATH', 'Archive backups to BACKUP_PATH') do |p|
+        options[:backup_path] = p
+      end
+
+      opts.on("-h", "--help", "Prints this help") do
+        puts opts
+        exit
+      end
+    end.parse!(ARGV.dup)
+
+    # backwards compatibility with the bare [accepts directory path]
+    # leftover_args will be ['gitlab','omnibus-ctl','backup-etc',[possible backup path],[extra bare options...]]
+    if options[:backup_path].nil? && leftover_args.size >= 4
+      log 'Specifying a custom backup path as a bare option is deprecated.'\
+          ' Please use the -p or --backup-path option to specify a custom backup path.'
+      options[:backup_path] = leftover_args[3]
+    end
+
+    options
+  end
+
+  GitlabCtl::Backup.perform(get_ctl_options)
 end
