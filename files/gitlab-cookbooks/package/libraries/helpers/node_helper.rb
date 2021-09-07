9cdb9fec8b841cda7553d9a125f92e956ccf0be4
Gabriel Mazetto <gabriel@gitlab.com>
2021-09-07 14:21:12 +0200

    Add tests to the NodeHelper

--- files/gitlab-cookbooks/package/libraries/helpers/node_helper.rb
+++ files/gitlab-cookbooks/package/libraries/helpers/node_helper.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
 module NodeHelper
-  def self.consume_cluster_attributes(node, attrs)
-    node.logger.debug("Applying attributes from Gitlab Cluster configuration")
-    node.override_attrs = merge_override!(node.override_attrs, attrs)
-  end
+  class << self
+    # Merges attributes into existing node with override level
+    #
+    # @param [Mash] node
+    # @param [Gitlab::Config::Mash] attrs
+    def consume_cluster_attributes(node, attrs)
+      node.logger.debug("Applying attributes from Gitlab Cluster configuration")
+      node.override_attrs = merge_override!(node.override_attrs, attrs)
+    end
 
-  def self.merge_override!(source, dest)
-    message = "The configured values from #{GitlabCluster::JSON_FILE} overrides the " \
+    private
+
+    def merge_override!(source, dest)
+      message = "The configured values from #{GitlabCluster::JSON_FILE} overrides the " \
               "the setting in the /etc/gitlab/gitlab.rb configuration file"
 
-    LoggingHelper.warning(message) unless dest.empty?
+      LoggingHelper.warning(message) unless dest.empty?
 
-    Chef::Mixin::DeepMerge.merge(source, dest)
+      Chef::Mixin::DeepMerge.merge(source, dest)
+    end
   end
 end
