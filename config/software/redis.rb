959791e8e013f411bdf645975a66782a3daf1119
Balasankar 'Balu' C <balasankar@gitlab.com>
2021-10-27 10:08:18 +0000

    Bump redis to 6.0.16

--- config/software/redis.rb
+++ config/software/redis.rb
@@ -23,7 +23,7 @@ license_file 'COPYING'
 skip_transitive_dependency_licensing true
 
 dependency 'config_guess'
-version = Gitlab::Version.new('redis', '6.0.14')
+version = Gitlab::Version.new('redis', '6.0.16')
 default_version version.print(false)
 
 source git: version.remote
