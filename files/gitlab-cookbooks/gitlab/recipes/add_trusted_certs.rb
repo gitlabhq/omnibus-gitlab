2df3c5d0a7b622706733b29c5fe2e20db6f3172c
Balasankar 'Balu' C <balasankar@gitlab.com>
2021-12-10 23:29:29 +0000

    Restart gitlab-workhorse when trusted certificates change

--- files/gitlab-cookbooks/gitlab/recipes/add_trusted_certs.rb
+++ files/gitlab-cookbooks/gitlab/recipes/add_trusted_certs.rb
@@ -36,4 +36,5 @@ ruby_block "Move existing certs and link to #{ssl_certs_dir}" do
   only_if { cert_helper.new_certificate_added? }
   notifies :restart, "runit_service[puma]" if omnibus_helper.should_notify?("puma")
   notifies :restart, "runit_service[gitlab-pages]" if omnibus_helper.should_notify?("gitlab-pages")
+  notifies :restart, "runit_service[gitlab-workhorse]" if omnibus_helper.should_notify?("gitlab-workhorse")
 end
