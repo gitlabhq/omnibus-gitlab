f84ce593a285d30d2d14e179282ee1618370950d
Balasankar "Balu" C <balasankar@gitlab.com>
2022-05-19 11:45:21 +0530

    Use different package name for FIPS builds
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

---
+++ config/projects/gitlab/gitlab-fips.rb
@@ -0,0 +1,4 @@
+name 'gitlab-fips'
+description 'GitLab Enterprise Edition (including NGINX, Postgres, Redis) with FIPS compliance'
+maintainer 'GitLab, Inc. <support@gitlab.com>'
+homepage 'https://about.gitlab.com/'
