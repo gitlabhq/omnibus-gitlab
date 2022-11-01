8cfcfb3747d46c43f7e5c2fb1fec107609a32515
Hossein Pursultani <hpursultani@gitlab.com>
2022-11-01 11:47:14 +0000

    Upgrade curl to 7.85.0
    Merge branch 'security-curl-7.85-for-15.4' into '15-4-stable'
    
    See merge request gitlab-org/security/omnibus-gitlab!246
    
    Changelog: security

--- config/software/curl.rb
+++ config/software/curl.rb
@@ -16,7 +16,7 @@
 
 name 'curl'
 
-version = Gitlab::Version.new('curl', 'curl-7_79_1')
+version = Gitlab::Version.new('curl', 'curl-7_85_0')
 
 default_version version.print(false)
 display_version version.print(false).delete_prefix('curl-').tr('_', '.')
