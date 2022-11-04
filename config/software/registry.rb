b48b492d2effeccd03fea52a752a59a64e5261ae
Hayley Swimelar <hswimelar@gitlab.com>
2022-11-04 20:54:29 +0000

    Bump Container Registry to v3.60.1-gitlab
    Related https://gitlab.com/gitlab-org/container-registry/-/issues/821
    
    Changelog: changed

--- config/software/registry.rb
+++ config/software/registry.rb
@@ -19,7 +19,7 @@
 require "#{Omnibus::Config.project_root}/lib/gitlab/version"
 
 name 'registry'
-version = Gitlab::Version.new('registry', 'v3.57.0-gitlab')
+version = Gitlab::Version.new('registry', 'v3.60.1-gitlab')
 
 default_version version.print(false)
 display_version version.print(false).delete_suffix('-gitlab')
