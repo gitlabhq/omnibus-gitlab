4f40dfd1cc3b856542a0d8e992e659ae91c1ea0b
Balasankar 'Balu' C <balasankar@gitlab.com>
2022-11-09 02:55:52 +0000

    Require necessary libraries in simple project configuration
    Add references to several shared ruby components used to build
    software components in the simple project definition.
    
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- doc/development/examples/simple.rb
+++ doc/development/examples/simple.rb
@@ -1,3 +1,10 @@
+require "#{Omnibus::Config.project_root}/lib/gitlab/build/info"
+require "#{Omnibus::Config.project_root}/lib/gitlab/build_iteration"
+require "#{Omnibus::Config.project_root}/lib/gitlab/ohai_helper.rb"
+require "#{Omnibus::Config.project_root}/lib/gitlab/openssl_helper"
+require "#{Omnibus::Config.project_root}/lib/gitlab/util"
+require "#{Omnibus::Config.project_root}/lib/gitlab/version"
+
 name 'simple'
 description 'Simple project to test omnibus changes'
 
