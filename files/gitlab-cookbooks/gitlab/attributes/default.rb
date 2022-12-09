b6ac13b198e31f3f60cc2f51cb1fbd223960780e
Sean McGivern <sean@gitlab.com>
2022-12-09 15:07:21 +0000

    Set Sidekiq default max concurrency to 20
    The previous default of 50 was far too high. We don't use this on
    GitLab.com and it's mostly inertia that has left it at this value.
    
    Closes https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/5797
    
    Changelog: changed

--- files/gitlab-cookbooks/gitlab/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab/attributes/default.rb
@@ -595,7 +595,7 @@ default['gitlab']['sidekiq']['health_checks_listen_port'] = 8092
 # Cluster specific settings
 default['gitlab']['sidekiq']['queue_selector'] = false
 default['gitlab']['sidekiq']['interval'] = nil
-default['gitlab']['sidekiq']['max_concurrency'] = 50
+default['gitlab']['sidekiq']['max_concurrency'] = 20
 default['gitlab']['sidekiq']['min_concurrency'] = nil
 default['gitlab']['sidekiq']['negate'] = false
 default['gitlab']['sidekiq']['queue_groups'] = ['*']
