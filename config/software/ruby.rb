4526b529f61e619b93fa5cb6ef1cd6e76f127718
Stan Hu <stanhu@gmail.com>
2022-12-13 14:48:15 -0800

    Add Ruby 3.0.5 workaround for CentOS 7 and SLES
    See https://bugs.ruby-lang.org/issues/19161

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -58,6 +58,9 @@ env['CFLAGS'] << " -DOPENSSL_FIPS" if Build::Check.use_system_ssl?
 
 env['CFLAGS'] << ' -O3 -g -pipe'
 
+# Workaround for https://bugs.ruby-lang.org/issues/19161
+env['CFLAGS'] << ' -std=gnu99' if OhaiHelper.get_centos_version.to_i == 7 || OhaiHelper.os_platform == 'sles'
+
 build do
   env['CFLAGS'] << ' -fno-omit-frame-pointer'
   # Fix for https://bugs.ruby-lang.org/issues/18409. This can be removed with Ruby 3.0+.
