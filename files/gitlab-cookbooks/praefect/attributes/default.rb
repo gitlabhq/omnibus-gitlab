38a0e35faa362fd7fd914fcee5d287edbb2f186c
Hossein Pursultani <hpursultani@gitlab.com>
2023-02-02 03:03:05 +0000

    Revert "Merge branch 'smh-generic-praefect-templating' into 'master'"
    This reverts merge request !6373

--- files/gitlab-cookbooks/praefect/attributes/default.rb
+++ files/gitlab-cookbooks/praefect/attributes/default.rb
@@ -1,23 +1,47 @@
 default['praefect']['enable'] = false
+default['praefect']['failover_enabled'] = true
+default['praefect']['failover_timeout'] = nil
+default['praefect']['failover_election_strategy'] = nil
+default['praefect']['auth_token'] = nil
+default['praefect']['auth_transitioning'] = false
 default['praefect']['dir'] = "/var/opt/gitlab/praefect"
 default['praefect']['log_directory'] = "/var/log/gitlab/praefect"
 default['praefect']['env_directory'] = "/opt/gitlab/etc/praefect/env"
 # default['praefect']['env'] is set in ../recipes/enable.rb
 default['praefect']['wrapper_path'] = "/opt/gitlab/embedded/bin/gitaly-wrapper"
+default['praefect']['listen_addr'] = "localhost:2305"
+default['praefect']['prometheus_grpc_latency_buckets'] = nil
+default['praefect']['prometheus_listen_addr'] = "localhost:9652"
+default['praefect']['logging_level'] = nil
+default['praefect']['logging_format'] = 'json'
+default['praefect']['sentry_dsn'] = nil
+default['praefect']['sentry_environment'] = nil
+default['praefect']['virtual_storages'] = {}
 default['praefect']['auto_migrate'] = true
+default['praefect']['database_host'] = nil
+default['praefect']['database_port'] = nil
+default['praefect']['database_user'] = nil
+default['praefect']['database_password'] = nil
+default['praefect']['database_dbname'] = nil
+default['praefect']['database_sslmode'] = nil
+default['praefect']['database_sslcert'] = nil
+default['praefect']['database_sslkey'] = nil
+default['praefect']['database_sslrootcert'] = nil
+default['praefect']['database_direct_host'] = nil
+default['praefect']['database_direct_port'] = nil
+default['praefect']['database_direct_user'] = nil
+default['praefect']['database_direct_password'] = nil
+default['praefect']['database_direct_dbname'] = nil
+default['praefect']['database_direct_sslmode'] = nil
+default['praefect']['database_direct_sslcert'] = nil
+default['praefect']['database_direct_sslkey'] = nil
+default['praefect']['database_direct_sslrootcert'] = nil
+default['praefect']['separate_database_metrics'] = true
+default['praefect']['tls_listen_addr'] = nil
+default['praefect']['certificate_path'] = nil
+default['praefect']['key_path'] = nil
 default['praefect']['consul_service_name'] = 'praefect'
 default['praefect']['consul_service_meta'] = nil
-default['praefect']['configuration'] = {
-  listen_addr: 'localhost:2305',
-  prometheus_listen_addr: 'localhost:9652',
-  logging: {
-    format: 'json',
-  },
-  prometheus_exclude_database_from_default_metrics: true,
-  auth: {
-    transitioning: false
-  },
-  failover: {
-    enabled: true
-  }
-}
+default['praefect']['background_verification_verification_interval'] = nil
+default['praefect']['background_verification_delete_invalid_records'] = nil
+default['praefect']['graceful_stop_timeout'] = nil
