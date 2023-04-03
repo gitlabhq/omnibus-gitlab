dfbf2bfceedc97df5ee2696069f3a453d444a9ba
Clemens Beck <cbeck@gitlab.com>
2023-04-03 12:52:40 +0000

    Fix consul cleanup removes service files
    The consul cleanup mistakenly removed service configurations.
    It is only meant to cleanup excess watcher configs and scripts.
    
    Relates: https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/6747
    Relates: https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/7293
    Fixes:   https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/7717

--- spec/chef/cookbooks/consul/recipes/consul_watcher_spec.rb
+++ spec/chef/cookbooks/consul/recipes/consul_watcher_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe 'consul::watchers' do
   let(:chef_run) { ChefSpec::SoloRunner.converge('gitlab-ee::default') }
   let(:watcher_conf) { '/var/opt/gitlab/consul/config.d/watcher_postgresql.json' }
   let(:watcher_check) { '/var/opt/gitlab/consul/scripts/failover_postgresql_in_pgbouncer' }
+  let(:any_service_conf) { '/var/opt/gitlab/consul/config.d/any_service.conf' }
   let(:excess_watcher_conf) { '/var/opt/gitlab/consul/config.d/watcher_postgresql_old.json' }
   let(:excess_watcher_check) { '/var/opt/gitlab/consul/scripts/failover_postgresql_old_in_pgbouncer' }
 
   def stub_excess_consul_config_files
     allow(Dir).to receive(:glob).with(anything).and_call_original
-    allow(Dir).to receive(:glob).with("/var/opt/gitlab/consul/config.d/*").and_return [excess_watcher_conf]
+    allow(Dir).to receive(:glob).with("/var/opt/gitlab/consul/config.d/*").and_return [excess_watcher_conf, any_service_conf]
     allow(Dir).to receive(:glob).with("/var/opt/gitlab/consul/scripts/*").and_return [excess_watcher_check]
   end
 
@@ -48,6 +49,7 @@ RSpec.describe 'consul::watchers' do
   it 'removes config and watch files of removed watchers' do
     expect(chef_run).to delete_file(excess_watcher_conf)
     expect(chef_run).to delete_file(excess_watcher_check)
+    expect(chef_run).to_not delete_file(any_service_conf)
   end
 
   it 'creates the watcher handler file' do
