e236a4f3f4c8affc4b7767b601e023fa51c2d907
Robert Marshall <rmarshall@gitlab.com>
2023-06-12 21:33:05 +0000

    Merge branch '7761-fix-latest-stable-tag' into 'master'
    Move most of the latest tag detection logic to Ruby
    
    Closes #7761
    
    See merge request https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/6917
    
    Merged-by: Robert Marshall <rmarshall@gitlab.com>
    Approved-by: Jason Young <jyoung@gitlab.com>
    Approved-by: Robert Marshall <rmarshall@gitlab.com>
    Co-authored-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/gitlab/build/info.rb
+++ lib/gitlab/build/info.rb
@@ -10,8 +10,6 @@ require_relative 'image'
 
 module Build
   class Info
-    TagsNotFoundError = Class.new(StandardError)
-
     DEPLOYER_OS_MAPPING = {
       'AUTO_DEPLOY_ENVIRONMENT' => 'ubuntu-xenial',
       'PATCH_DEPLOY_ENVIRONMENT' => 'ubuntu-bionic',
@@ -73,6 +71,10 @@ module Build
         "#{semver}-#{Gitlab::BuildIteration.new.build_iteration}"
       end
 
+      def sorted_tags_for_edition
+        `git -c versionsort.prereleaseSuffix=rc tag -l '#{Info.tag_match_pattern}' --sort=-v:refname`.split("\n")
+      end
+
       # TODO, merge latest_tag with latest_stable_tag
       # TODO, add tests, needs a repo clone
       def latest_tag
@@ -80,9 +82,16 @@ module Build
           return fact_from_file
         end
 
-        version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
+        tags = sorted_tags_for_edition
 
-        `git -c versionsort.prereleaseSuffix=rc tag -l '#{version}#{Info.tag_match_pattern}' --sort=-v:refname | head -1`.chomp
+        return if tags.empty?
+
+        version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
+        output = tags.find { |t| t.start_with?(version) } if version
+
+        # If no tags corresponding to the stable branch version was found, we
+        # fall back to the latest available tag
+        output || tags.first
       end
 
       def latest_stable_tag(level: 1)
@@ -90,21 +99,33 @@ module Build
           return fact_from_file
         end
 
+        # Exclude RC tags so that we only have stable tags.
+        stable_tags = sorted_tags_for_edition.reject { |t| t.include?('rc') }
+
+        return if stable_tags.empty?
+
         version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
+
+        results = stable_tags.select { |t| t.start_with?(version) } if version
+
+        # If no tags corresponding to the stable branch version was found, we
+        # fall back to the latest available stable tag
+        output = if results.nil? || results.empty?
+                   stable_tags
+                 else
+                   results
+                 end
 
         # Level decides tag at which position you want. Level one gives you
         # latest stable tag, two gives you the one just before it and so on.
-        output = `git -c versionsort.prereleaseSuffix=rc tag -l '#{version}#{Info.tag_match_pattern}' --sort=-v:refname | awk '!/rc/' | head -#{level}`&.split("\n")&.last
-
-        # If no tags exist that start with the specified version, we need to
-        # fall back to the available latest stable tag. For that, we run the
-        # same command without the version in the filter argument.
-        raise TagsNotFoundError if output.nil?
-
-        output
-      rescue TagsNotFoundError
-        puts "No tags found in #{version}.x series. Falling back to latest available tag."
-        `git -c versionsort.prereleaseSuffix=rc tag -l '#{Info.tag_match_pattern}' --sort=-v:refname | awk '!/rc/' | head -#{level}`.split("\n").last
+        # Since arrays start from 0, we subtract 1 from the specified level to
+        # get the index. If the specified level is more than the number of
+        # tags, we return the last tag.
+        if level >= output.length
+          output.last
+        else
+          output[level - 1]
+        end
       end
 
       def docker_tag
