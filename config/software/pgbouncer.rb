54708e89be9be8617cb4ff55e433fbee28d3d603
Stan Hu <stanhu@gmail.com>
2023-06-14 14:48:24 -0700

    Build PgBouncer from Git source
    This makes it possible to automate the dependencies updates since we
    don't have to update a checksum. Previously the release source tarball
    was downloaded.
    
    This also adds some redundancy if the GitHub binary can't be
    downloaded since we now have mirrors for this repository.
    
    This came out of
    https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/6933.
    
    Changelog: changed

--- config/software/pgbouncer.rb
+++ config/software/pgbouncer.rb
@@ -15,7 +15,8 @@
 #
 
 name 'pgbouncer'
-default_version '1.18.0'
+version = Gitlab::Version.new('pgbouncer', 'pgbouncer_1_18_0')
+default_version version.print(false)
 
 license 'ISC'
 license_file 'COPYRIGHT'
@@ -25,24 +26,24 @@ skip_transitive_dependency_licensing true
 dependency 'libevent'
 dependency 'openssl' unless Build::Check.use_system_ssl?
 
-version '1.18.0' do
-  source sha256: '9349c9e59f6f88156354f4f6af27cdb014a235b00ae184cbaa37688bd0df544c'
-end
-
-source url: "https://www.pgbouncer.org/downloads/files/#{version}/pgbouncer-#{version}.tar.gz"
-
-relative_path "pgbouncer-#{version}"
+source git: version.remote
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+  cwd = "#{Omnibus::Config.source_dir}/pgbouncer"
+
+  command %w[git submodule init], cwd: cwd
+  command %w[git submodule update], cwd: cwd
+  command './autogen.sh', env: env, cwd: cwd
 
   prefix = "#{install_dir}/embedded"
-
   configure_command = ["./configure", "--prefix=#{prefix}", "--with-libevent=#{prefix}"]
   configure_command << "--with-openssl=#{prefix}" unless Build::Check.use_system_ssl?
+  command configure_command.join(' '), env: env, cwd: cwd
 
-  command configure_command.join(' '), env: env
+  # Disable building of docs to avoid the need for pandoc
+  command 'sed -i -e "/^dist_man_MANS =/d" Makefile', env: env, cwd: cwd
 
-  make "-j #{workers}", env: env
-  make 'install', env: env
+  make "-j #{workers}", env: env, cwd: cwd
+  make 'install', env: env, cwd: cwd
 end
