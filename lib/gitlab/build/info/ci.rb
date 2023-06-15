e23fd167211c98035c07ee43ef0001185eb80d6b
Balasankar "Balu" C <balasankar@gitlab.com>
2023-06-15 12:25:54 +0530

    Add generic methods to fetch URL to download artifact from API
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/gitlab/build/info/ci.rb
+++ lib/gitlab/build/info/ci.rb
@@ -33,6 +33,49 @@ module Build
         def api_v4_url
           Gitlab::Util.get_env('CI_API_V4_URL')
         end
+
+        def artifact_url(job_name, file_path)
+          client = Gitlab::APIClient.new
+          target_job_id = client.get_job_id(job_name)
+
+          return unless target_job_id
+
+          URI("#{api_v4_url}/projects/#{project_id}/jobs/#{target_job_id}/artifacts/#{file_path}")
+        end
+
+        def triggered_package_download_url(fips: Build::Check.use_system_ssl?)
+          folder = fips ? 'ubuntu-focal_fips' : 'ubuntu-jammy'
+          job_name = fips ? 'Trigger:package:fips' : 'Trigger:package'
+          package_path = "pkg/#{folder}/gitlab.deb"
+
+          artifact_url(job_name, package_path)
+        end
+
+        def package_download_url(job_name: "Ubuntu-22.04", arch: 'amd64')
+          case job_name
+          when /AlmaLinux-8/
+            # In EL world, amd64 is called x86_64
+            arch = 'x86_64' if arch == 'amd64'
+            folder = 'el-8'
+            package_file_name = "#{Info.package}-#{Info.release_version.gsub('+', '%2B')}.el8.#{arch}.rpm"
+          when /Ubuntu-20.04/
+            folder = 'ubuntu-focal'
+            package_file_name = "#{Info.package}_#{Info.release_version.gsub('+', '%2B')}_#{arch}.deb"
+          when /Ubuntu-22.04/
+            folder = 'ubuntu-jammy'
+            package_file_name = "#{Info.package}_#{Info.release_version.gsub('+', '%2B')}_#{arch}.deb"
+          end
+
+          if arch == 'arm64'
+            job_name = "#{job_name}-arm64"
+            folder = "#{folder}_aarch64"
+          end
+
+          job_name = "#{job_name}-branch" unless Build::Check.on_tag?
+
+          package_path = "pkg/#{folder}/#{package_file_name}"
+          Build::Info::CI.artifact_url(job_name, package_path)
+        end
       end
     end
   end
