33ebeb2cf7081f72b596401a5b4cd6c70f2961fd
DJ Mountney <dj@gitlab.com>
2023-07-20 15:01:16 +0000

    Merge branch 'split-info-git' into 'master'
    Split Git related information to own class
    
    See merge request https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/7008
    
    Merged-by: DJ Mountney <dj@gitlab.com>
    Approved-by: Andrew Patterson <apatterson@gitlab.com>
    Approved-by: DJ Mountney <dj@gitlab.com>
    Co-authored-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/gitlab/build/info.rb
+++ lib/gitlab/build/info.rb
@@ -2,6 +2,7 @@ require 'omnibus'
 require 'net/http'
 require 'json'
 
+require_relative 'info/git'
 require_relative '../build_iteration'
 require_relative "../util.rb"
 require_relative './info/ci'
@@ -18,13 +19,6 @@ module Build
     PACKAGE_GLOB = "pkg/**/*.{deb,rpm}".freeze
 
     class << self
-      def fetch_fact_from_file(fact)
-        return unless File.exist?("build_facts/#{fact}")
-
-        content = File.read("build_facts/#{fact}").strip
-        return content unless content.empty?
-      end
-
       def package
         return "gitlab-fips" if Check.use_system_ssl?
         return "gitlab-ee" if Check.is_ee?
@@ -48,84 +42,18 @@ module Build
           Omnibus.load_configuration('omnibus.rb')
           Omnibus::BuildVersion.semver
         else
-          latest_git_tag = Info.latest_tag.strip
+          latest_git_tag = Info::Git.latest_tag.strip
           latest_version = latest_git_tag && !latest_git_tag.empty? ? latest_git_tag[0, latest_git_tag.match("[+]").begin(0)] : '0.0.1'
-          commit_sha = Build::Info.commit_sha
+          commit_sha = Build::Info::Git.commit_sha
           ver_tag = "#{latest_version}+" + (Build::Check.is_nightly? ? "rnightly" : "rfbranch")
           ver_tag += ".fips" if Build::Check.use_system_ssl?
           [ver_tag, Gitlab::Util.get_env('CI_PIPELINE_ID'), commit_sha].compact.join('.')
         end
       end
 
-      def branch_name
-        Gitlab::Util.get_env('CI_COMMIT_BRANCH')
-      end
-
-      def commit_sha
-        commit_sha_raw = Gitlab::Util.get_env('CI_COMMIT_SHA') || `git rev-parse HEAD`.strip
-        commit_sha_raw[0, 8]
-      end
-
       def release_version
         semver = Info.semver_version
         "#{semver}-#{Gitlab::BuildIteration.new.build_iteration}"
-      end
-
-      def sorted_tags_for_edition
-        `git -c versionsort.prereleaseSuffix=rc tag -l '#{Info.tag_match_pattern}' --sort=-v:refname`.split("\n")
-      end
-
-      # TODO, merge latest_tag with latest_stable_tag
-      # TODO, add tests, needs a repo clone
-      def latest_tag
-        unless (fact_from_file = fetch_fact_from_file(__method__)).nil?
-          return fact_from_file
-        end
-
-        tags = sorted_tags_for_edition
-
-        return if tags.empty?
-
-        version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
-        output = tags.find { |t| t.start_with?(version) } if version
-
-        # If no tags corresponding to the stable branch version was found, we
-        # fall back to the latest available tag
-        output || tags.first
-      end
-
-      def latest_stable_tag(level: 1)
-        unless (fact_from_file = fetch_fact_from_file(__method__)).nil?
-          return fact_from_file
-        end
-
-        # Exclude RC tags so that we only have stable tags.
-        stable_tags = sorted_tags_for_edition.reject { |t| t.include?('rc') }
-
-        return if stable_tags.empty?
-
-        version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
-
-        results = stable_tags.select { |t| t.start_with?(version) } if version
-
-        # If no tags corresponding to the stable branch version was found, we
-        # fall back to the latest available stable tag
-        output = if results.nil? || results.empty?
-                   stable_tags
-                 else
-                   results
-                 end
-
-        # Level decides tag at which position you want. Level one gives you
-        # latest stable tag, two gives you the one just before it and so on.
-        # Since arrays start from 0, we subtract 1 from the specified level to
-        # get the index. If the specified level is more than the number of
-        # tags, we return the last tag.
-        if level >= output.length
-          output.last
-        else
-          output[level - 1]
-        end
       end
 
       def docker_tag
@@ -162,12 +90,6 @@ module Build
         #    anyway pointing to immutable references (git tags), and hence we
         #    can directly use it.
         Gitlab::Version.new('gitlab-rails').print(prepend_version)
-      end
-
-      def previous_version
-        # Get the second latest git tag
-        previous_tag = Info.latest_stable_tag(level: 2)
-        previous_tag.tr("+", "-")
       end
 
       def gitlab_rails_project_path
@@ -325,10 +247,6 @@ module Build
         contents << "DOWNLOAD_URL=#{download_url}\n"
         contents << "CI_JOB_TOKEN=#{Build::Info::CI.job_token}\n"
         contents.join
-      end
-
-      def current_git_tag
-        `git describe --exact-match 2>/dev/null`.chomp
       end
 
       def image_reference
---
+++ lib/gitlab/build/info/git.rb
@@ -0,0 +1,104 @@
+require_relative '../../util'
+require_relative '../check'
+require_relative 'ci'
+
+module Build
+  class Info
+    class Git
+      class << self
+        def branch_name
+          # If on CI, branch name from `CI_COMMIT_BRANCH` wins
+          result = Build::Info::CI.branch_name
+
+          return result if result
+
+          # If not on CI, attempt to detect branch name
+          head_reference = Gitlab::Util.shellout_stdout('git rev-parse --abbrev-ref HEAD')
+
+          # On tags, the shell command will return `HEAD`. If that is not the
+          # case, we are on a branch and can return the output we received.
+          return head_reference unless head_reference == "HEAD"
+        end
+
+        def tag_name
+          Build::Info::CI.tag_name || Gitlab::Util.shellout_stdout('git describe --tags --exact-match')
+        rescue Gitlab::Util::ShellOutExecutionError => e
+          return nil if /fatal: no tag exactly matches/.match?(e.stderr)
+
+          raise "#{e.message}\nSTDOUT: #{e.stdout}\nSTDERR: #{e.stderr}"
+        end
+
+        def commit_sha
+          commit_sha_raw = Gitlab::Util.get_env('CI_COMMIT_SHA') || Gitlab::Util.shellout_stdout('git rev-parse HEAD')
+
+          commit_sha_raw[0, 8]
+        end
+
+        # TODO, merge latest_tag with latest_stable_tag
+        # TODO, add tests, needs a repo clone
+        def latest_tag
+          unless (fact_from_file = Gitlab::Util.fetch_fact_from_file(__method__)).nil?
+            return fact_from_file
+          end
+
+          tags = sorted_tags_for_edition
+
+          return if tags.empty?
+
+          version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
+          output = tags.find { |t| t.start_with?(version) } if version
+
+          # If no tags corresponding to the stable branch version was found, we
+          # fall back to the latest available tag
+          output || tags.first
+        end
+
+        def latest_stable_tag(level: 1)
+          unless (fact_from_file = Gitlab::Util.fetch_fact_from_file(__method__)).nil?
+            return fact_from_file
+          end
+
+          # Exclude RC tags so that we only have stable tags.
+          stable_tags = sorted_tags_for_edition.reject { |t| t.include?('rc') }
+
+          return if stable_tags.empty?
+
+          version = branch_name.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch?
+
+          results = stable_tags.select { |t| t.start_with?(version) } if version
+
+          # If no tags corresponding to the stable branch version was found, we
+          # fall back to the latest available stable tag
+          output = if results.nil? || results.empty?
+                     stable_tags
+                   else
+                     results
+                   end
+
+          # Level decides tag at which position you want. Level one gives you
+          # latest stable tag, two gives you the one just before it and so on.
+          # Since arrays start from 0, we subtract 1 from the specified level to
+          # get the index. If the specified level is more than the number of
+          # tags, we return the last tag.
+          if level >= output.length
+            output.last
+          else
+            output[level - 1]
+          end
+        end
+
+        private
+
+        def sorted_tags_for_edition
+          Gitlab::Util.shellout_stdout("git -c versionsort.prereleaseSuffix=rc tag -l '#{tag_match_pattern}' --sort=-v:refname")&.split("\n")
+        end
+
+        def tag_match_pattern
+          return '*[+.]ee.*' if Build::Check.is_ee?
+
+          '*[+.]ce.*'
+        end
+      end
+    end
+  end
+end
