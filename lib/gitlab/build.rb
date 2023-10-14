10db300868952fd72145b65fec772692d26f70b7
Balasankar "Balu" C <balasankar@gitlab.com>
2023-10-14 11:41:47 +0530

    Move Build::Info.log_level to Build.log_level
    This information is used only during building. So it makes sense for it
    to stay in the Build module.
    
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/gitlab/build.rb
+++ lib/gitlab/build.rb
@@ -1,11 +1,19 @@
-require_relative "build/info.rb"
+require_relative "util"
 
 module Build
+  def self.log_level
+    if Gitlab::Util.get_env('BUILD_LOG_LEVEL') && !Gitlab::Util.get_env('BUILD_LOG_LEVEL').empty?
+      Gitlab::Util.get_env('BUILD_LOG_LEVEL')
+    else
+      'info'
+    end
+  end
+
   def self.exec(project)
     system(*cmd(project))
   end
 
   def self.cmd(project)
-    %W[bundle exec omnibus build #{project} --log-level #{Info.log_level}]
+    %W[bundle exec omnibus build #{project} --log-level #{log_level}]
   end
 end
--- lib/gitlab/build/info.rb
+++ lib/gitlab/build/info.rb
@@ -5,17 +5,3 @@ require_relative './info/ci'
 require_relative './info/package'
 require_relative 'check'
 require_relative 'image'
-
-module Build
-  class Info
-    class << self
-      def log_level
-        if Gitlab::Util.get_env('BUILD_LOG_LEVEL') && !Gitlab::Util.get_env('BUILD_LOG_LEVEL').empty?
-          Gitlab::Util.get_env('BUILD_LOG_LEVEL')
-        else
-          'info'
-        end
-      end
-    end
-  end
-end
