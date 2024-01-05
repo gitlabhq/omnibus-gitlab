6ec0bf3afbffd1824eba7d7c4244d5ec087868d4
Clemens Beck <cbeck@gitlab.com>
2024-01-05 18:39:11 +0100

    Fix upgrade check comparison
    Convert major and minor version from string to int to compare them as
    intended.
    
    Changelog: fixed

--- files/gitlab-ctl-commands/lib/gitlab_ctl/upgrade_check.rb
+++ files/gitlab-ctl-commands/lib/gitlab_ctl/upgrade_check.rb
@@ -5,11 +5,11 @@ module GitlabCtl
         # If old_version is nil, this is a fresh install
         return true if ov.nil?
 
-        old_version_major = ov.split('.')[0]
-        old_version_minor = ov.split('.')[1]
+        old_version_major = ov.split('.')[0].to_i
+        old_version_minor = ov.split('.')[1].to_i
         min_version = min_version()
-        min_version_major = min_version.split('.')[0]
-        min_version_minor = min_version.split('.')[1]
+        min_version_major = min_version.split('.')[0].to_i
+        min_version_minor = min_version.split('.')[1].to_i
 
         minimum_version_detected = old_version_major == min_version_major && old_version_minor >= min_version_minor
         minimum_version_detected || old_version_major > min_version_major
