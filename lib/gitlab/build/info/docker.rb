ca02e0c220f009a7b0758bb32ff43d20b448d52f
Balasankar 'Balu' C <balasankar@gitlab.com>
2024-03-20 15:09:24 +0530

    Enable building arm64 images
    Modify the Dockerfile and library code around Docker image builds to
    support building multiarch images. The Dockerfile will download the
    package for the correct architecture based on TARGETARCH variable.
    
    Closes: https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/8470
    
    Signed-off-by: Balasankar 'Balu' C <balasankar@gitlab.com>

--- lib/gitlab/build/info/docker.rb
+++ lib/gitlab/build/info/docker.rb
@@ -13,15 +13,19 @@ module Build
 
         def release_file_contents
           repo = Gitlab::Util.get_env('PACKAGECLOUD_REPO') # Target repository
+          download_urls = {}.tap do |urls|
+            urls[:amd64] = Build::Info::CI.package_download_url
+            urls[:arm64] = Build::Info::CI.package_download_url(arch: 'arm64')
+          end
 
-          download_url = Build::Info::CI.package_download_url
-          raise "Unable to identify package download URL." unless download_url
+          raise "Unable to identify package download URLs." if download_urls.empty?
 
           contents = []
           contents << "PACKAGECLOUD_REPO=#{repo.chomp}\n" if repo && !repo.empty?
           contents << "RELEASE_PACKAGE=#{Build::Info::Package.name}\n"
           contents << "RELEASE_VERSION=#{Build::Info::Package.release_version}\n"
-          contents << "DOWNLOAD_URL=#{download_url}\n"
+          contents << "DOWNLOAD_URL_amd64=#{download_urls[:amd64]}\n"
+          contents << "DOWNLOAD_URL_arm64=#{download_urls[:arm64]}\n"
           contents << "CI_JOB_TOKEN=#{Build::Info::CI.job_token}\n"
           contents.join
         end
