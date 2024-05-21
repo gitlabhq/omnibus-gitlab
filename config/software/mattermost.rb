0185f82b5f76a1b8bf0742b5c3636de3dcf4d999
Andrew Patterson <apatterson@gitlab.com>
2024-05-21 19:38:18 +0000

    Mattermost 2024-04 security update
    Merge branch 'security_mattermost_update_2024-04-16.10' into '16-10-stable'
    
    See merge request gitlab-org/security/omnibus-gitlab!427
    
    Changelog: security

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -18,10 +18,10 @@
 
 name 'mattermost'
 
-default_version '9.5.1'
+default_version '9.5.4'
 
 source url: "https://releases.mattermost.com/#{version}/mattermost-team-#{version}-linux-amd64.tar.gz",
-       sha256: '17e73b98a6b874b90a4da7ac5b7cead9d0892ec82d066f692c2aefb639cf658e'
+       sha256: 'd40e9f12150e3609d2e815da8e63f45207de95c72c5dc5e5047f9648b139e2ee'
 relative_path 'mattermost'
 
 license_name = 'GITLAB-MATTERMOST-COMPILED-LICENSE.txt'
