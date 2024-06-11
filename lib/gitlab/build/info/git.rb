142bf6601c2fd16bc9f0c45b28d2dc0d12d90f8e
Stan Hu <stanhu@gmail.com>
2024-06-11 09:59:17 -0700

    Fix stable version tag identification for 16.1.x releases
    In https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/7681
    we found that the `upgrade-minor` QA job was attempting to upgrade
    from 16.10 to 16.11 for a merge request targeting `16-1-stable`.  This
    occurred because the version matching check only found a stable branch
    that started with `16.1`, so `16.11.3` was a valid match.
    
    Fix this by tightening the match to include a period, so that `16.1.`
    will match `16.1.7`.
    
    Changelog: fixed

--- lib/gitlab/build/info/git.rb
+++ lib/gitlab/build/info/git.rb
@@ -57,7 +57,7 @@ module Build
           branch_for_version = Build::Info::CI.mr_target_branch_name || branch_name
 
           version = branch_for_version.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch? || Build::Check.mr_targetting_stable_branch?
-          output = tags.find { |t| t.start_with?(version) } if version
+          output = tags.find { |t| t.start_with?("#{version}.") } if version
 
           # If no tags corresponding to the stable branch version was found, we
           # fall back to the latest available tag
@@ -78,7 +78,7 @@ module Build
 
           version = branch_for_version.delete_suffix('-stable').tr('-', '.') if Build::Check.on_stable_branch? || Build::Check.mr_targetting_stable_branch?
 
-          results = stable_tags.select { |t| t.start_with?(version) } if version
+          results = stable_tags.select { |t| t.start_with?("#{version}.") } if version
 
           # If no tags corresponding to the stable branch version was found, we
           # fall back to the latest available stable tag
