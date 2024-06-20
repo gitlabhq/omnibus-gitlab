bdf7221985c60390a90a1cd0295bb29c007b55b0
Stan Hu <stanhu@gmail.com>
2024-06-19 17:43:32 -0700

    Force ffi gem to use Ruby platform gem
    ffi v1.17.0 started shipping precompiled native gems for x86 and
    aarch64 Linux. However, these precompiled gems don't include
    the Omnibus library path (`/opt/gitlab/embedded/lib`) as part
    of the default `LD_LIBRARY_PATH`, so libraries such as `libarchive`
    can't be loaded from Omnibus.
    
    Bundler allows us to ignore the precompiled gems by using
    `force_ruby_platform` in the `Gemfile`, so use this feature to
    effectively restore the behavior in previous ffi gem versions.
    
    Relates to https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/8590
    
    Changelog: fixed

--- config/software/omnibus-gitlab-gems.rb
+++ config/software/omnibus-gitlab-gems.rb
@@ -18,7 +18,7 @@
 require "#{Omnibus::Config.project_root}/lib/gitlab/ohai_helper.rb"
 name 'omnibus-gitlab-gems'
 
-default_version '20240110'
+default_version '20240619'
 
 license 'MIT'
 license_file 'LICENSE'
