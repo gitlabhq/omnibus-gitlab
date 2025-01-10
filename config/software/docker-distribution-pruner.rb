c8e94eecaa766f2b2525d348b6166a5dc2421b3f
Stan Hu <stanhu@gmail.com>
2025-01-10 09:30:57 -0800

    Bump docker-distribution-pruner to v0.3.0
    This version upgrade:
    
    * Requires Go 1.22 and higher
    * Bumps an outdated golang.org/x/net version
    
    This project is defunct, but some scanners will might find that CVEs
    from the old dependency.
    
    Changelog: changed

--- config/software/docker-distribution-pruner.rb
+++ config/software/docker-distribution-pruner.rb
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 require "#{Omnibus::Config.project_root}/lib/gitlab/version"
-version = Gitlab::Version.new('docker-distribution-pruner', '0.2.0')
+version = Gitlab::Version.new('docker-distribution-pruner', '0.3.1')
 
 name 'docker-distribution-pruner'
 default_version version.print
