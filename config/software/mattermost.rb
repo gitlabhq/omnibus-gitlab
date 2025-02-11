413e46fd42b8bbca0751e7f6ddfb2e9c6d6e7980
Clemens Beck <cbeck@gitlab.com>
2025-02-11 17:45:46 +0000

    Mattermost Security Updates January 22, 2025
    Merge branch 'security-update-mm-jan-2025-17-7' into '17-7-stable'
    
    See merge request gitlab-org/security/omnibus-gitlab!460
    
    Changelog: security

--- config/software/mattermost.rb
+++ config/software/mattermost.rb
@@ -18,10 +18,10 @@
 
 name 'mattermost'
 
-default_version '10.2.0'
+default_version '10.2.3'
 
 source url: "https://releases.mattermost.com/#{version}/mattermost-team-#{version}-linux-amd64.tar.gz",
-       sha256: 'c060ad8bc6d437c54b0badeffc752bc986e16759049a4282a8e49b1bae47d0d1'
+       sha256: 'b18ad8db0ae03c863fa4cb7f611584794776e9a1df52cf19c4bb90ce86938eef'
 relative_path 'mattermost'
 
 license_name = 'GITLAB-MATTERMOST-COMPILED-LICENSE.txt'
