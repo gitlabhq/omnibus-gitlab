0d10f5cda9b70c47637a21ac69e90b453728fbde
Balasankar 'Balu' C <balasankar@gitlab.com>
2025-05-06 14:44:18 +0530

    Mark minimum required PostgreSQL version to be 16
    - Upgrades to GitLab greater than version 17.11 require
      PostgreSQL to be on version 16 minimum.
    
    Changelog: changed
    
    Signed-off-by: Balasankar 'Balu' C <balasankar@gitlab.com>

--- files/gitlab-cookbooks/gitlab/recipes/database_migrations.rb
+++ files/gitlab-cookbooks/gitlab/recipes/database_migrations.rb
@@ -35,10 +35,10 @@ env_variables['GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN'] = initial_runner_token
 ruby_block "check remote PG version" do
   block do
     remote_db_version = GitlabRailsEnvHelper.db_version
-    if remote_db_version && remote_db_version.to_f < 14
+    if remote_db_version && remote_db_version.to_f < 16
       LoggingHelper.warning(%q(
-        Note that PostgreSQL 14 is the minimum required PostgreSQL version in GitLab 17.0.
-        Support for PostgreSQL 13 has been removed.
+        Note that PostgreSQL 16 is the minimum required PostgreSQL version in GitLab 18.0.
+        Support for PostgreSQL 14 and 15 have been removed.
         To upgrade, please see: https://docs.gitlab.com/omnibus/settings/database.html#upgrade-packaged-postgresql-server
       ))
     end
