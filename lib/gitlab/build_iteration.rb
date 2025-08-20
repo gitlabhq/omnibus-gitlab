aea28a5fa10c6a8d929a2ce898a8c9dcbf4d6b03
Lucas Li <lli@gitlab.com>
2025-08-20 18:02:03 +0900

    Refactor to use Build::Info::Git.tag_name instead of git describe
    Closes: https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/9265

--- lib/gitlab/build_iteration.rb
+++ lib/gitlab/build_iteration.rb
@@ -3,14 +3,10 @@ require_relative 'build/check'
 # This module has tests in spec/gitlab/build_iteration_spec.rb
 module Gitlab
   class BuildIteration
-    def initialize(git_describe = nil)
-      @git_describe = git_describe || `git describe`
-      @git_describe = @git_describe.chomp
-    end
-
     def build_iteration
       if Build::Check.on_tag?
-        match = /[^+]*\+([^\-]*)/.match(@git_describe)
+        tag_name = Build::Info::Git.tag_name
+        match = /[^+]*\+([^\-]*)/.match(tag_name)
         if match && !match[1].empty?
           result = match[1]
           result = result.gsub('ee.', 'fips.') if Build::Check.use_system_ssl?
