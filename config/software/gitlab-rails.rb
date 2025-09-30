9a5a4adfee666f35b9fff1d7ce9abe0349ef1b7f
Stan Hu <stanhu@gmail.com>
2025-09-30 01:05:14 -0700

    Revert "Ignore musl precompiled libraries for tailwindcss and lightningcss"
    This reverts commit 7066021e30aa13f856964fef70eaf0fafd2a1a97.

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -60,13 +60,6 @@ dependency 'exiftool'
 if EE
   dependency 'pgbouncer'
   dependency 'patroni'
-
-  # yarn installs both glibc and musl versions, and nodejs dynamically
-  # chooses between them at runtime. It's not easy to exclude the musl
-  # binaries without yarn v2, so let's just ignore these files
-  # because they should never be used.
-  whitelist_file %r{#{install_dir}/embedded/service/gitlab-rails/ee/frontend_islands/apps/duo_next/node_modules/lightningcss-linux-.*-musl/lightningcss\.linux-.*-musl\.node}
-  whitelist_file %r{#{install_dir}/embedded/service/gitlab-rails/ee/frontend_islands/apps/duo_next/node_modules/@tailwindcss/oxide-linux-.*-musl/tailwindcss-oxide\.linux-.*-musl\.node}
 end
 
 # libatomic is a runtime_dependency of the grpc gem for armhf/aarch64 platforms
@@ -348,4 +341,6 @@ build do
 
   command "#{install_dir}/embedded/bin/ruby #{install_dir}/embedded/bin/gitlab-gem-license-generator"
   delete "#{install_dir}/embedded/bin/gitlab-gem-license-generator"
+
+  delete "#{install_dir}/embedded/service/gitlab-rails/ee/frontend_islands/apps/duo_next/node_modules" if EE
 end
