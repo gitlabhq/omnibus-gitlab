7236e421d0a944cf3189facb1637b488035da36a
Stefan Constantin <sconstantin@gitlab.com>
2026-06-22 07:33:59 +0200

    Re-enable OpenSUSE 15.6 builds
    - Backports https://gitlab.com/gitlab-org/omnibus-gitlab/-/merge_requests/9473
    - Re-enables SLES 15.6 support in omnibus-gitlab up to 19.5
    
    Changelog: changed

--- config/software/gitaly.rb
+++ config/software/gitaly.rb
@@ -56,14 +56,6 @@ build do
 
   git_cflags = '-fno-omit-frame-pointer'
 
-  # SLES uses gcc v4.8.5 with C90 (`-std=gnu90`) by default.
-  # C11 is a newer standard than C90, and gcc v5.1.0 switched the default
-  # from `std=gnu90` to `std=gnu11`.
-  # Git v2.35 added a balloon test that will fail the build if
-  # C99 is not supported. On other platforms, C11 may be required
-  # (https://gitlab.com/gitlab-org/gitlab-git/-/commit/7bc341e21b5).
-  git_cflags += ' -std=gnu99' if OhaiHelper.os_platform == 'sles'
-
   git_append_build_options = [
     "# Added by Omnibus git software definition gitaly.rb",
     "GIT_APPEND_BUILD_OPTIONS += ICONVDIR=#{install_dir}/embedded",
