efddd13c85ceba0cf1b6a8c293eda08034011a55
Bo Carbonell <bcarbonell@gitlab.com>
2026-07-23 15:19:59 +0000

    Fail on deprecated Mattermost settings only if Mattermost is enabled
    changelog: fixed

--- spec/chef/cookbooks/package/libraries/deprecations_spec.rb
+++ spec/chef/cookbooks/package/libraries/deprecations_spec.rb
@@ -347,10 +347,24 @@ RSpec.describe Gitlab::Deprecations do
       allow(Gitlab::Deprecations).to receive(:list).and_call_original
     end
 
-    it 'reports any mattermost[*] key as removed in 19.0' do
+    it 'reports active Mattermost config as removed in 19.0' do
       config = { 'mattermost' => { 'enable' => true } }
       messages = described_class.check_config('19.0', config, :removal)
       expect(messages).to include(a_string_matching(/mattermost has been deprecated since 19\.0 and was removed in 19\.0/))
+    end
+
+    it 'does not report stale secrets-only Mattermost cached state as removed in 19.0' do
+      config = {
+        'mattermost' => {
+          'email_invite_salt' => 'abc',
+          'file_public_link_salt' => 'def',
+          'sql_at_rest_encrypt_key' => 'ghi',
+          'register_as_oauth_app' => false
+        }
+      }
+
+      messages = described_class.check_config('19.0', config, :removal)
+      expect(messages).not_to include(a_string_matching(/mattermost has been deprecated since 19\.0 and was removed in 19\.0/))
     end
 
     it 'warns when mattermost_external_url is set in 19.0' do
